029755f2bc1147ba2c04db181c0950422a828d03
kgkali <ali.asger@kgkmail.com>
2023-09-05 14:38:29 +0530

    implement side-scrollable component and it's testcases

---
+++ src/components/common/side-scrollable/example/example.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { CustomSideScrollable } from '..';
+import styles from './sideScroller.module.scss';
+
+export const SideScrollableExample = () => {
+  return (
+    <>
+      <CustomSideScrollable
+        leftFixedStyle={styles.leftSideStyle}
+        leftFixedContent={
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum maxime aliquid laudantium voluptates sapiente in, error unde pariatur qui ad dolorum corporis distinctio labore vel nulla quam non, consequatur, est molestias praesentium beatae impedit eligendi delectus. Modi animi dicta alias, cupiditate totam esse consequuntur aspernatur quaerat! Consequuntur perferendis ab blanditiis, eveniet non debitis necessitatibus ad, odio explicabo distinctio animi architecto est. Aut ut rem impedit eos at possimus exercitationem adipisci. Quidem tempore praesentium qui ipsa enim aliquam repellat, magni, minima reiciendis vel asperiores totam sequi! Exercitationem dolorum, sed maiores neque illum, officiis dolores voluptate alias obcaecati quae saepe temporibus ducimus perferendis aspernatur sapiente molestias impedit tenetur odio quibusdam ab, ipsa adipisci a quas accusantium! Voluptatum alias doloremque quidem libero? Odio, molestiae! Expedita ducimus ab deleniti hic, quod magnam animi odio nisi dolor ipsum adipisci natus blanditiis obcaecati architecto. Odit assumenda quibusdam corrupti libero placeat aperiam obcaecati nobis tenetur veritatis numquam, laborum quaerat officia alias hic dolore itaque tempora porro repellat quasi rerum id. Repudiandae aut, praesentium, nihil accusantium animi non soluta, tempore tenetur sed totam atque impedit beatae provident? Ratione quasi ipsam eos, quibusdam ex nulla magnam assumenda suscipit magni sint cumque, esse hic fuga et reprehenderit perspiciatis laudantium non.'
+        }
+        rightSideContent={
+          <>
+            <div className="flex">
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.cardTitle}>Happy Birthday!</div>
+                <div className={styles.cardMessage}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  possimus illo qui voluptas reprehenderit maiores provident
+                  placeat, dignissimos labore voluptatibus eligendi asperiores
+                  ipsa vitae, quam ea neque ad maxime! Eligendi eveniet sit
+                  dolore ex dolores, ipsa odit placeat non maiores ipsum et
+                  velit officiis quasi tempore at esse aspernatur? Officiis non
+                  delectus odit voluptates similique qui? Commodi praesentium
+                  rem vero, voluptas quam doloremque non provident quo nostrum
+                  re
+                </div>
+                <div className={styles.senderName}>- Your Name</div>
+              </div>
+            </div>
+          </>
+        }
+      />
+    </>
+  );
+};
---
+++ src/components/common/side-scrollable/index.tsx
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from 'react';
+import styles from './sideScrollable.module.scss';
+// import './test.css';
+
+interface ISideScrollableProps {
+  leftFixedStyle?: string;
+  leftFixedContent: React.ReactNode;
+  rightSideStyle?: string;
+  rightSideContent: React.ReactNode;
+}
+
+export const CustomSideScrollable: React.FC<ISideScrollableProps> = ({
+  leftFixedStyle,
+  leftFixedContent,
+  rightSideStyle,
+  rightSideContent,
+}) => {
+  const scrollableRef = useRef<HTMLDivElement | null>(null);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [startX, setStartX] = useState<number | null>(null);
+  const [scrollLeft, setScrollLeft] = useState<number | null>(null);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    if (scrollableRef.current) {
+      setStartX(e.pageX - scrollableRef.current.offsetLeft);
+      setScrollLeft(scrollableRef.current.scrollLeft);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging || !scrollableRef.current) return;
+    e.preventDefault();
+    const x = e.pageX - scrollableRef.current.offsetLeft;
+    const walk = (x - (startX as number)) * 2; // Adjust the scroll speed here
+    scrollableRef.current.scrollLeft = (scrollLeft as number) - walk;
+  };
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    if (scrollableRef.current) {
+      setScrollLeft(scrollableRef.current.scrollLeft);
+    }
+  };
+  return (
+    <div className="flex">
+      <div className={`${leftFixedStyle}`}>{leftFixedContent}</div>
+      <div
+        role="scrollable-container"
+        className={styles.scrollableContainer}
+        ref={scrollableRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseUp}
+      >
+        <div className={styles.scrollText} data-testid="scrollable-container">
+          {rightSideContent}
+        </div>
+      </div>
+    </div>
+  );
+};
