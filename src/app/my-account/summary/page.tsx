f37af73812efefbf233b2eb4ec14e62270ae56c2
Jyoti Tiwari <jyoti.tiwari@kgkmail.com>
2023-09-12 15:05:43 +0530

    adding summary page card

--- src/app/my-account/summary/page.tsx
+++ src/app/my-account/summary/page.tsx
@@ -1,11 +1,18 @@
 import { CustomInputlabel } from '@/components/common/input-label';
+import { ManageLocales } from '@/utils/translate';
 import React from 'react';
-
+import style from './summary.module.scss'
 const Summary = () => {
   return (
     <div>
-    <CustomInputlabel htmlfor={''} label={'Personal Information'}/>
-    <div></div>
+    <CustomInputlabel htmlfor={''} label={ManageLocales('app.myaccount.summary.personalInformation')}/>
+    <div className={style.personalCard}>
+      <div className={style.personalCardHeader}>Aman</div>
+      <div>
+        <p className={style.personalCardData}>{ManageLocales('app.myaccount.summary.contactNo')} : +919108766432</p>
+        <p className={style.personalCardData}>{ManageLocales('app.myaccount.summary.emailID')} : amanwilson@mail.com</p>
+      </div>
+    </div>
     </div>
   );
 };
