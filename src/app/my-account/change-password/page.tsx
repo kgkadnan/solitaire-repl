6c493cf44f41580846712b7e945cabd2f1b40b9a
Jyoti Tiwari <jyoti.tiwari@kgkmail.com>
2023-09-12 18:35:53 +0530

    added states to handle toggle of password

--- src/app/my-account/change-password/page.tsx
+++ src/app/my-account/change-password/page.tsx
@@ -1,16 +1,44 @@
-import { CustomInputField } from '@/components/common/input-field'
-import React from 'react'
-import style from './change-password.module.scss'
+"use client"
+import { CustomInputField } from '@/components/common/input-field';
+import React, { useState } from 'react';
+import style from './change-password.module.scss';
 const ChangePassword = () => {
+  const [currentPasswordVisible,setCurrentPasswordVisible] =useState(true)
+  const [newPasswordVisible,setNewPasswordVisible] =useState(false)
+  const [confirmPasswordVisible,setConfirmPasswordVisible] =useState(false)
   return (
     <div className={style.changePassword}>
-        <div className={style.changePasswordContainer}>
-          <CustomInputField type={'text'} name={'current password'} placeholder='Current Password' style={{inputMain:style.inputContainerStyles,input:style.inputStyles}}/>
-          <CustomInputField type={'text'} name={'current password'} placeholder='Current Password' style={{inputMain:style.inputContainerStyles,input:style.inputStyles}}/>
-          <CustomInputField type={'text'} name={'current password'} placeholder='Current Password' style={{inputMain:style.inputContainerStyles,input:style.inputStyles}}/>
-        </div>
+      <div className={style.changePasswordContainer}>
+        <CustomInputField
+           type={`${currentPasswordVisible? 'text':'password'}`}
+          name={'current password'}
+          placeholder="Current Password"
+          style={{
+            inputMain: style.inputContainerStyles,
+            input: style.inputStyles,
+          }}
+        />
+        <CustomInputField
+          type={`${newPasswordVisible? 'text':'password'}`}
+          name={'new password'}
+          placeholder="New Password"
+          style={{
+            inputMain: style.inputContainerStyles,
+            input: style.inputStyles,
+          }}
+        />
+        <CustomInputField
+           type={`${confirmPasswordVisible? 'text':'password'}`}
+          name={'confirm password'}
+          placeholder="Confirm Password"
+          style={{
+            inputMain: style.inputContainerStyles,
+            input: style.inputStyles,
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
