384c27877e14c222c8bd799ddad7ed766ff920d0
kgkali <ali.asger@kgkmail.com>
2023-09-26 14:31:45 +0530

    calculator file name change and test cases improved

--- src/__tests__/components/calculator.test.tsx
+++ src/__tests__/components/calculator.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { CustomCalculator } from '@/components/caclulator/calculator';
+import { CustomCalculator } from '@/components/caclulator';
 
 describe('CustomCalculator Component', () => {
-  // Mock click handler
-  let onItemClick = jest.fn();
+
 
   // Common test setup
   beforeEach(() => {
@@ -15,26 +14,26 @@ describe('CustomCalculator Component', () => {
     expect(screen.getByText('Your Diamond')).toBeInTheDocument();
   });
 
-  // test('updates the count when the up/down icons are clicked', () => {
-  //   const upIcon = screen.getByTestId('up-icon');
-  //   const downIcon = screen.getByTestId('down-icon');
-  //   const countValue = screen.getByTestId('countValue');
+  test('updates the count when the up/down icons are clicked', () => {
+    const upIcon = screen.getByTestId('up-icon');
+    const downIcon = screen.getByTestId('down-icon');
+    const countValue = screen.getByTestId('countValue');
 
-  //   // Check if the initial count value is 0
-  //   expect(countValue).toHaveValue('0');
+    // Check if the initial count value is 0
+    expect(countValue).toHaveValue(0);
 
-  //   // Simulate a click on the Up icon
-  //   fireEvent.click(upIcon);
+    // Simulate a click on the Up icon
+    fireEvent.click(upIcon);
 
-  //   // Check if the count value is updated to 1
-  //   expect(countValue).toHaveValue('1');
+    // Check if the count value is updated to 1
+    expect(countValue).toHaveValue(1);
 
-  //   // Simulate a click on the Down icon
-  //   fireEvent.click(downIcon);
+    // Simulate a click on the Down icon
+    fireEvent.click(downIcon);
 
-  //   // Check if the count value is updated back to 0
-  //   expect(countValue).toHaveValue('0');
-  // });
+    // Check if the count value is updated back to 0
+    expect(countValue).toHaveValue(0);
+  });
 
   test('handles input changes', () => {
     const input = screen.getByPlaceholderText('99.99');
