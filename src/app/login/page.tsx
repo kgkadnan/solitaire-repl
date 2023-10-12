6be65b534b26165c2656e8bca48ad9d8de73db05
Jyoti Tiwari <jyotitiwari334@gmail.com>
2023-10-12 21:40:22 +0530

    added screen

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import roundImg from '@public/assets/images/Roundbig.png';
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+            hi
+        </div>
+    </div>
+  )
+}
+
+export default Login
