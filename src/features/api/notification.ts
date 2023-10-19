9495d20f0c1abc5c555bb88b78a2cb34c7b41de5
kgkali <ali.asger@kgkmail.com>
2023-10-19 10:50:33 +0530

    in compare stone add to cart api implement and error handling

--- src/features/api/notification.ts
+++ src/features/api/notification.ts
@@ -3,7 +3,14 @@ const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const notificationApi = createApi({
   reducerPath: 'notificationReducer',
-  baseQuery: fetchBaseQuery({ baseUrl: apiURL, credentials: 'include' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: apiURL,
+    credentials: 'include',
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcl9pZCI6ImN1c18wMUhEMFI2NFZHQThLNFcyNkFORlZDUTQ1TSIsImRvbWFpbiI6InN0b3JlIiwiaWF0IjoxNjk3NjExNzEzLCJleHAiOjE3MDAyMDM3MTN9.X_ETyZIozIhg5bm7kcu2jRxG500sCHfl98eESyBQtHo',
+    },
+  }),
   tagTypes: ['notification'],
 
   endpoints: (builder) => ({
