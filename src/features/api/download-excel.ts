19cf60d560b38366bffe7345b25ac2ce1cdc746f
Jyoti Tiwari <jyotitiwari334@gmail.com>
2023-10-23 23:48:04 +0530

    add saved search multiple from advance search and created slice to maintain saved searches

--- src/features/api/download-excel.ts
+++ src/features/api/download-excel.ts
@@ -7,7 +7,7 @@ export const downloadExcelApi = createApi({
     baseUrl: apiURL,
     headers: {
       Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcl9pZCI6ImN1c18wMUhEMFI2NFZHQThLNFcyNkFORlZDUTQ1TSIsImRvbWFpbiI6InN0b3JlIiwiaWF0IjoxNjk3NjExNzEzLCJleHAiOjE3MDAyMDM3MTN9.X_ETyZIozIhg5bm7kcu2jRxG500sCHfl98eESyBQtHo',
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcl9pZCI6ImN1c18wMUhERUtLMVQyUlNXNUpIVzdERlNUWjdEVCIsImRvbWFpbiI6InN0b3JlIiwiaWF0IjoxNjk4MDc2MTYwLCJleHAiOjE3MDA2NjgxNjB9.6Yzsnd8xXUVb_FuhgCpi77WuXBjUKUrcKgxb_RsCxe4',
     },
   }),
   tagTypes: ['downloadExcel'],
