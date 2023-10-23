f555444fc0ee28886aad0ccfd5ed0b7046902157
Adnan <adnan@belgiumwebnet.com>
2023-10-23 10:21:47 +0530

    minor changes on adv search page

--- src/components/common/dialog/index.tsx
+++ src/components/common/dialog/index.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-import confirmImage from '@public/assets/icons/confirmation.svg';
-import Image from 'next/image';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 interface IDialog {
   dialogContent: React.ReactNode;
-  isOpens? : boolean
+  isOpens?: boolean;
 }
 
 export const CustomDialog: React.FC<IDialog> = ({
   dialogContent,
-  isOpens=false
+  isOpens = false,
 }) => {
   return (
     <>
       <Dialog open={isOpens}>
         {/* <DialogTrigger>{dialogTrigger}</DialogTrigger> */}
         <DialogContent className="sm:max-w-[400px] h-[200px] bg-solitaireSecondary z-[1200]">
-          {/* <div className="max-w-[400px] flex justify-center align-middle">
-            <Image src={confirmImage} alt="vector image" />
-          </div> */}
-          {/* <div className="max-w-[400px] flex justify-center align-middle text-solitaireTertiary"> */}
-            {dialogContent}
-          {/* </div> */}
+          {dialogContent}
         </DialogContent>
       </Dialog>
     </>
