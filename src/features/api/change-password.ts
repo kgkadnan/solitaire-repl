1d057b198fb65d6c650b7c9e5c3ca449ae35d052
kgkali <ali.asger@kgkmail.com>
2023-10-27 12:55:22 +0530

    Change-password API and state management implement

---
+++ src/features/api/change-password.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+// const header =
+export const changePasswordApi = createApi({
+  reducerPath: 'changePasswordReducer',
+  baseQuery: fetchBaseQuery({
+    baseUrl: apiURL,
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcl9pZCI6ImN1c18wMUhEMFI2NFZHQThLNFcyNkFORlZDUTQ1TSIsImRvbWFpbiI6InN0b3JlIiwiaWF0IjoxNjk3NjExNzEzLCJleHAiOjE3MDAyMDM3MTN9.X_ETyZIozIhg5bm7kcu2jRxG500sCHfl98eESyBQtHo',
+    },
+  }),
+  tagTypes: ['changePassword'],
+
+  endpoints: (builder) => ({
+    ChangePassword: builder.mutation({
+      query: (data) => ({
+        url: `change-password`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['changePassword'],
+    }),
+  }),
+});
+
+export const { useChangePasswordMutation } = changePasswordApi;
