9fa9a9947a652597945f9c06de65d0faa774d351
kgkali <ali.asger@kgkmail.com>
2023-12-11 10:40:57 +0530

    create build

--- src/app/my-diamonds/my-diamonds-interface.ts
+++ src/app/my-diamonds/my-diamonds-interface.ts
@@ -1,21 +1,21 @@
 export interface IHeaderSearchBarProps {
-    activeTab: string;
-    setRecentConfiramtionSearchUrl: (url: string) => void;
-    setMyInvoiceSearchUrl: (url: string) => void;
-    setPreviousConfirmationSearchUrl: (url: string) => void;
-  }
+  activeTab: string;
+  setRecentConfiramtionSearchUrl: (url: string) => void;
+  setMyInvoiceSearchUrl: (url: string) => void;
+  setPreviousConfirmationSearchUrl: (url: string) => void;
+}
 
- export interface ITabsProps {
-    activeTab: string;
-    setActiveTab: (tab: string) => void;
-    recentConfirmCount: number;
-    myInvoiceDataCount: number;
-    previousConfirmCount: number;
-  }
+export interface ITabsProps {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+  recentConfirmCount: number;
+  myInvoiceDataCount: number;
+  previousConfirmCount: number;
+}
 
-  export interface IFilterByDaysProps {
-    activeTab: string;
-    setRecentConfirmationSelectedDays: (selectedDays: string) => void;
-    setMyInvoiceSelectedDays: (selectedDays: string) => void;
-    setPreviousConfirmationSelectedDays: (selectedDays: string) => void;
-  }
+export interface IFilterByDaysProps {
+  activeTab: string;
+  setRecentConfirmationSelectedDays: (selectedDays: string) => void;
+  setMyInvoiceSelectedDays: (selectedDays: string) => void;
+  setPreviousConfirmationSelectedDays: (selectedDays: string) => void;
+}
