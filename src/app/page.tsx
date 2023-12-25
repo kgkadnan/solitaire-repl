441db406c08b60f7bc2a8e73cf40cd40bb5b2409
Adnan <adnan@belgiumwebnet.com>
2023-12-25 09:56:49 +0530

    otp input created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
 'use client';
 
-import OtpInput from '@/components/common/otp-verification';
-import { useState } from 'react';
-
 export default function Home() {
   // const logger = getLogger('home');
   // logger.error('a error message from _app');
   // logger.debug('a debug message from _app');
-  const [otpValues, setOtpValues] = useState<string[]>([
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
-  ]);
-  const correctOtp = '123456'; // Example correct OTP. In a real application, this should be dynamically obtained.
-
-  const verifyOtp = () => {
-    const enteredOtp = otpValues.join('');
-    if (enteredOtp === correctOtp) {
-      alert('OTP Verified Successfully!');
-      // Additional logic for successful verification
-    } else {
-      alert('Incorrect OTP. Please try again.');
-      // Additional logic for failed verification
-    }
-  };
 
   return (
     <>
@@ -50,10 +27,6 @@ export default function Home() {
       >
         Building Digital Diamond Platform
       </h1>
-      <OtpInput setOtpValues={setOtpValues} otpValues={otpValues} />
-      <button onClick={verifyOtp} className="verify-button">
-        Verify
-      </button>
     </>
   );
 }
