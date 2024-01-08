f73c1e1025c5475f1f753499ac092aafe2cf65f5
kgkali <ali.asger@kgkmail.com>
2024-01-08 17:41:47 +0530

    build created and fixes

--- src/components/common/stepper/example.tsx
+++ src/components/common/stepper/example.tsx
@@ -62,9 +62,9 @@ const StepperExample = () => {
       setState={setCurrentStep}
       prevStep={prevStep}
       nextStep={nextStep}
-      // setIsDialogOpen={() => ''}
-      // isDialogOpen={false}
-      // dialogContent={'test'}
+      setIsDialogOpen={() => ''}
+      isDialogOpen={false}
+      dialogContent={'test'}
     />
   );
 };
