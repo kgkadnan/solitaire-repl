c6bf779bcbf81033fd8b8bda0fb6e71c84b6808d
Jyoti Tiwari <jyoti.tiwari@kgkmail.com>
2024-01-19 18:21:18 +0530

    tooltip component added with chevorn arrow

--- src/app/v2/form/form.tsx
+++ src/app/v2/form/form.tsx
@@ -1,7 +1,14 @@
+"use client"
+import Tooltip from '@/components/v2/common/tooltip';
 import React from 'react';
 
 const Form = () => {
-  return <div>Form</div>;
+  return <div>Form
+<Tooltip tooltipTrigger={<div>jjjj</div>}
+tooltipContent={<div>jjjj</div>}
+/>
+
+  </div>;
 };
 
 export default Form;
