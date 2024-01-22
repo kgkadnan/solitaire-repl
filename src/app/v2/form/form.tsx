9594c5b636c0c22f3c85b6a64cea3dd650286f59
kgkadnan <adnan.badshah@kgkmail.com>
2024-01-22 17:49:32 +0530

    accordion implemented

--- src/app/v2/form/form.tsx
+++ src/app/v2/form/form.tsx
@@ -1,9 +1,86 @@
 'use client';
 
+import { AccordionComponent } from '@/components/v2/common/accordion';
+import AnchorLinkNavigation from '@/components/v2/common/anchor-tag-navigation';
 import React from 'react';
 
 const Form = () => {
-  return <>form</>;
+  let linkItems = ['a', 'b', 'c', 'd', 'f', 'g', 'h'];
+  return (
+    <>
+      <AccordionComponent
+        value="shape"
+        isDisable={true}
+        accordionContent={<>Hello</>}
+        accordionTrigger={'shape'}
+        hasError={false}
+      />
+      <AccordionComponent
+        value="parameter"
+        isDisable={false}
+        accordionContent={<>Hello</>}
+        accordionTrigger={'parameter'}
+        hasError={false}
+      />
+      <AnchorLinkNavigation linkItems={linkItems} />
+      <p className="mt-10" id="a">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="b">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="c">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="d">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="f">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="g">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+      <p className="mt-10" id="h">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
+        maxime adipisci, eaque repellendus minus doloribus, animi molestiae
+        asperiores quod, deleniti veritatis accusamus. Aliquid ut optio libero
+        fugiat dignissimos corporis explicabo, autem consectetur natus vitae,
+        totam amet fugit dolore? Incidunt numquam dolorum ipsum id beatae quo
+        reprehenderit saepe, cum esse quisquam mollitia tempore sunt reiciendis
+      </p>
+    </>
+  );
 };
 
 export default Form;
--- src/components/v2/common/accordion/index.tsx
+++ src/components/v2/common/accordion/index.tsx
@@ -1,62 +1,55 @@
-// import {
-//   Accordion,
-//   AccordionContent,
-//   AccordionItem,
-//   AccordionTrigger
-// } from '@/components/v2/ui/accordion';
-// import styles from './accordion.module.scss';
-// import { useRef } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/v2/ui/accordion';
+import styles from './accordion.module.scss';
+import essentials from '@public/assets/icons/Essentials.svg';
+import Image from 'next/image';
 
-// interface IAccordionCompProps {
-//   isDisable?: boolean;
-//   accordionContent: React.ReactNode;
-//   accordionTrigger: string;
-//   value: string;
-//   hasError: boolean;
-// }
+interface IAccordionCompProps {
+  isDisable?: boolean;
+  accordionContent: React.ReactNode;
+  accordionTrigger: string;
+  value: string;
+  hasError: boolean;
+}
 
-// export const AccordionComp: React.FC<IAccordionCompProps> = ({
-//   isDisable = false,
-//   accordionContent,
-//   accordionTrigger,
-//   value,
-//   hasError
-// }) => {
-//   const accordionTriggerRef = useRef(null);
-
-//   const checkIsErrorExist = hasError => {
-//     console.log('hasErrir');
-//   };
-
-//   const onValueChanged = () => {
-//     const accordionTriggerElement = accordionTriggerRef.current;
-//     const dataState = accordionTriggerElement.getAttribute('data-state');
-
-//     if (dataState === 'closed') {
-//       checkIsErrorExist(hasError);
-//     }
-//     console.log('dataState', dataState);
-//   };
-
-//   return (
-//     <Accordion
-//       type="single"
-//       className="w-full"
-//       defaultValue={value}
-//       collapsible
-//       onValueChange={onValueChanged}
-//     >
-//       <AccordionItem value={value} ref={accordionTriggerRef}>
-//         <AccordionTrigger
-//           className={styles.accordionTriggerStyle}
-//           disabled={isDisable}
-//         >
-//           {accordionTrigger}
-//         </AccordionTrigger>
-//         <AccordionContent className={styles.accordionContentStyle}>
-//           {accordionContent}
-//         </AccordionContent>
-//       </AccordionItem>
-//     </Accordion>
-//   );
-// };
+export const AccordionComponent: React.FC<IAccordionCompProps> = ({
+  isDisable = false,
+  accordionContent,
+  accordionTrigger,
+  value,
+  hasError
+}) => {
+  return (
+    <Accordion
+      type="single"
+      className="w-full"
+      defaultValue={value}
+      collapsible
+    >
+      <AccordionItem value={value}>
+        <AccordionTrigger
+          className={`${
+            hasError ? styles.accordionErrorStyle : styles.accordionSuccessStyle
+          } ${styles.accordionTriggerStyle}`}
+          disabled={isDisable}
+        >
+          <div className="flex items-center gap-2">
+            {hasError ? <Image src={essentials} alt="essentials" /> : null}
+            {accordionTrigger}
+          </div>
+        </AccordionTrigger>
+        <AccordionContent
+          className={`${
+            hasError ? styles.accordionErrorStyle : styles.accordionSuccessStyle
+          } ${styles.accordionContentStyle}`}
+        >
+          {accordionContent}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
