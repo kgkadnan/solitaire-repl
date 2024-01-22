5d54d5c456bb41792832ad6fc2453193d4e3e481
Jyoti Tiwari <jyoti.tiwari@kgkmail.com>
2024-01-22 18:14:46 +0530

    build fix

--- src/components/v2/common/image-tile/index.tsx
+++ src/components/v2/common/image-tile/index.tsx
@@ -37,33 +37,36 @@ const ImageTile: React.FC<IImageContainerProps> = (
         const { src, title, short_name } = tileData;
 
         return (
-          <Tooltip
-            tooltipTrigger={
-              <div
-                key={`image-tile-data-${title}`}
-                role="img"
-                className={`px-[25px] py-[20px] border-[1px] bg-neutralShapeDefault  border-neutral50 grid gap-[8px] w-[93px] h-[106px] rounded-[8px] justify-center text-center hover:border-neutralShapeHover  ${
-                  selectedTile.includes(short_name)
-                    ? 'shadow-[0px_4px_8px_0px_rgba(0, 0, 0, 0.06)] backdrop-blur-[25px] border-neutralShapeSelected'
-                    : ''
-                }`}
-                onClick={() => {
-                  handleSelectTile(short_name);
-                }}
-              >
-                <Image
-                  src={src}
-                  alt={title}
-                  className="mx-auto justify-center"
-                />
+          <>
+            <Tooltip
+              tooltipTrigger={
+                <div
+                  key={`image-tile-data-${title}`}
+                  role="img"
+                  className={`px-[25px] py-[20px] border-[1px] bg-neutralShapeDefault  border-neutral50 grid gap-[8px] w-[93px] h-[106px] rounded-[8px] justify-center text-center hover:border-neutralShapeHover  ${
+                    selectedTile.includes(short_name)
+                      ? 'shadow-[0px_4px_8px_0px_rgba(0, 0, 0, 0.06)] backdrop-blur-[25px] border-neutralShapeSelected'
+                      : ''
+                  }`}
+                  onClick={() => {
+                    handleSelectTile(short_name);
+                  }}
+                >
+                  <Image
+                    src={src}
+                    alt={title}
+                    className="mx-auto justify-center"
+                  />
 
-                <span className="text-neutral400 text-sRegular font-regular">
-                  {title}
-                </span>
-              </div>
-            }
-            tooltipContent={title}
-          />
+                  <span className="text-neutral400 text-sRegular font-regular">
+                    {title}
+                  </span>
+                </div>
+              }
+              tooltipContent={title}
+              key={`image-tile-data-${title}`}
+            />
+          </>
         );
       })}
     </div>
