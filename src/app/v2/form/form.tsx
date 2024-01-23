ca104ac7855f6c543bdb67be0ee282d6cd96839a
kgkadnan <adnan.badshah@kgkmail.com>
2024-01-23 13:00:35 +0530

    tile integrated

--- src/app/v2/form/form.tsx
+++ src/app/v2/form/form.tsx
@@ -3,6 +3,7 @@
 import { AccordionComponent } from '@/components/v2/common/accordion';
 import AnchorLinkNavigation from '@/components/v2/common/anchor-tag-navigation';
 import ImageTile from '@/components/v2/common/image-tile';
+import Tile from '@/components/v2/common/tile';
 import { shape } from '@/constants/v2/form';
 import React, { useState } from 'react';
 
@@ -26,12 +27,50 @@ const Form = () => {
       setSelectedShape(prevSelectedShape => [...prevSelectedShape, shape]);
     }
   };
-  const handleClick = () => {
-    console.log("pp")
+
+  const [make, setMake] = useState<string[]>([]);
+  const [cut, setCut] = useState<string[]>([]);
+  const handleTileClick = ({
+    data,
+    selectedTile,
+    setSelectedTile
+  }: {
+    data: string;
+    selectedTile: string[];
+    setSelectedTile: React.Dispatch<React.SetStateAction<string[]>>;
+  }) => {
+    if (selectedTile.includes(data)) {
+      setSelectedTile(prevSelectedTile =>
+        prevSelectedTile.filter(selected => selected !== data)
+      );
+    } else {
+      setSelectedTile(prevSelectedTile => [...prevSelectedTile, data]);
+    }
   };
+
+  let tileData = ['3EX', '3EX+NON', '3VG+EX', '3G', '3F'];
+  let cutData = [
+    { title: 'Excellent', short_name: 'EX' },
+    { title: 'Very Good', short_name: 'VG' },
+    { title: 'Good', short_name: 'G' },
+    { title: 'Fair', short_name: 'F' }
+  ];
+
   return (
     <div>
       {' '}
+      <Tile
+        tileData={tileData}
+        selectedTile={make}
+        setSelectedTile={setMake}
+        handleTileClick={handleTileClick}
+      />
+      <Tile
+        tileData={cutData}
+        selectedTile={cut}
+        setSelectedTile={setCut}
+        handleTileClick={handleTileClick}
+      />
       <AnchorLinkNavigation linkItems={linkItems} />
       <div className="mt-10" id="shape">
         <AccordionComponent
---
+++ src/components/v2/common/tile/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import styles from './tile.module.scss';
+import Tooltip from '../tooltip';
+
+interface ITileProps {
+  tileData: string[] | { title: string; short_name: string }[];
+  handleTileClick: ({
+    data,
+    selectedTile,
+    setSelectedTile
+  }: {
+    data: string;
+    selectedTile: string[];
+    setSelectedTile: React.Dispatch<React.SetStateAction<string[]>>;
+  }) => void;
+  selectedTile: string[];
+  setSelectedTile: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const Tile = ({
+  tileData,
+  handleTileClick,
+  selectedTile,
+  setSelectedTile
+}: ITileProps) => {
+  return (
+    <div className={styles.tileContainer}>
+      {tileData.map((tile: string | { title: string; short_name: string }) => {
+        return (
+          <>
+            {typeof tile === 'string' ? (
+              <Button
+                key={tile}
+                onClick={() =>
+                  handleTileClick({ data: tile, selectedTile, setSelectedTile })
+                }
+                className={` ${styles.tileDefaultStyles}   ${
+                  selectedTile.includes(tile)
+                    ? styles.tileActiveStyle
+                    : `${styles.tileDisableStyle} ${styles.tileStyles}`
+                }`}
+              >
+                {tile}
+              </Button>
+            ) : (
+              <Tooltip
+                tooltipTrigger={
+                  <Button
+                    onClick={() =>
+                      handleTileClick({
+                        data: tile.short_name,
+                        selectedTile,
+                        setSelectedTile
+                      })
+                    }
+                    className={` ${styles.tileDefaultStyles}   ${
+                      selectedTile.includes(tile.short_name)
+                        ? styles.tileActiveStyle
+                        : `${styles.tileDisableStyle} ${styles.tileStyles}`
+                    }`}
+                  >
+                    {tile.short_name}
+                  </Button>
+                }
+                tooltipContent={tile.title}
+              />
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Tile;
