cee1b0f4fdf6b98b395e2f6cdbbaf442e1f390e0
Jyoti Tiwari <jyoti.tiwari@kgkmail.com>
2024-01-24 15:18:21 +0530

    interface issue fix

--- src/app/v2/form/helpers/generate-query-parameters.ts
+++ src/app/v2/form/helpers/generate-query-parameters.ts
@@ -63,12 +63,11 @@ interface IState {
   ratioTo: string;
   girdlePerFrom: string;
   girdlePerTo: string;
-  girdleFrom: string;
-  girdleTo: string;
   pavilionAngleFrom: string;
   pavilionAngleTo: string;
   starLengthFrom: string;
   starLengthTo: string;
+  selectedGirdle:string[]
 }
 
 // Define a function to generate query parameters based on the provided state
@@ -140,8 +139,7 @@ export const generateQueryParams = (state: IState) => {
     starLengthTo,
     selectedColor,
     selectedGirdle
-    // girdleTo,
-    // girdleFrom
+  
   } = state;
 
   // Initialize an empty object to store query parameters
