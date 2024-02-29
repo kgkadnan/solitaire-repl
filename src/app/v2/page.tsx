539d569ffe0d29325bbaf20354c644e350e0148a
Jyoti <jyoti.tiwari@kgkmail.com>
2024-02-29 14:45:45 +0530

    minor change

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import ContactCard from '@/components/v2/common/kam-card';
+import KAMCard from '@/components/v2/common/kam-card';
 
 const page = () => {
   return (
     <div>
       {' '}
-      <ContactCard
+      <KAMCard
         name="Mr. Rajeev Sinha"
         role="Key Account Manager"
         phoneNumber="+91 910 876 6432"
