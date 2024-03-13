490d88a6f09a82e5c027eb8c7cf6c2ba3c0fa7a4
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-03-13 21:44:23 +0530

    update gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ import { ThemeProviders } from './theme-providers';
 import Head from 'next/head';
 import { SocketManager, useSocket } from '@/hooks/v2/socket-manager';
 import logger from 'logging/log-util';
+// import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
 const store = setupStore();
 
@@ -61,8 +63,39 @@ export default function RootLayout({ children }: { children?: ReactNode }) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Inter"
         />
+
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W85XMPHM');
+        `
+          }}
+        />
+        <Script strategy="afterInteractive">
+          {' '}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+     })(window,document,'script','dataLayer', 'GTM-W85XMPHM');`}
+        </Script>
+        {/* <GoogleAnalytics gaId="GTM-W85XMPHM" /> */}
+        {/* <!-- End Google Tag Manager --> */}
       </Head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-W85XMPHM`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Provider store={store}>
           <ThemeProviders>
             {isV2Route ? (
