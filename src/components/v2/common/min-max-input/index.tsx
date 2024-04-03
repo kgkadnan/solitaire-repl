74b212bb42282c4902f59d143f740d0e6ecaece9
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-04-03 18:14:57 +0530

    min max to from to updated

--- src/components/v2/common/min-max-input/index.tsx
+++ src/components/v2/common/min-max-input/index.tsx
@@ -10,7 +10,7 @@ export const MinMaxInput = ({
   errorText,
   isShowError = true
 }: IMinMaxInput) => {
-  const minMaxData = ['min', 'max'];
+  const minMaxData = ['from', 'to'];
   const { minValue, minPlaceHolder, minOnchange } = minInputData;
   const { maxValue, maxPlaceHolder, maxOnchange } = maxInputData;
 
@@ -24,7 +24,7 @@ export const MinMaxInput = ({
             <div className="">
               <InputLabel
                 htmlFor={type}
-                label={type === 'min' ? 'Min' : 'Max'}
+                label={type === 'from' ? 'From' : 'To'}
                 styles="border-y-[1px] border-l-[1px] text-neutral500 border-neutral200 rounded-l-[4px] p-2 mb-[4px]"
               />
               <p className="h-1"></p>
@@ -32,9 +32,9 @@ export const MinMaxInput = ({
             <InputField
               type="number"
               name={type}
-              value={type === 'min' ? minValue : maxValue}
-              placeholder={type === 'min' ? minPlaceHolder : maxPlaceHolder}
-              onChange={type === 'min' ? minOnchange : maxOnchange}
+              value={type === 'from' ? minValue : maxValue}
+              placeholder={type === 'from' ? minPlaceHolder : maxPlaceHolder}
+              onChange={type === 'from' ? minOnchange : maxOnchange}
               styles={{
                 input: `h-full p-2 flex-grow block w-full min-w-0 rounded-r-sm text-mRegular shadow-[var(--input-shadow)] border-[1px] border-neutral200 w-[92px] rounded-r-[4px] rounded-l-[0px] focus:border-[1px] 
                 ${errorText ? 'border-dangerMain' : 'border-neutral200'}`
