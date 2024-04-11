4c873dcf8733544b9b2834c781ab468690a7e803
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-04-11 21:41:22 +0530

    dna page changes

--- src/components/v2/common/detail-page/components/CommonTable.tsx
+++ src/components/v2/common/detail-page/components/CommonTable.tsx
@@ -26,11 +26,11 @@ const ResponsiveTable: React.FC<ResponsiveTableProps> = ({
     <div className="overflow-x-auto" style={{ borderRadius: '4px' }}>
       <table className="divide-y divide-[#E4E7EC] border  border-[#E4E7EC] overflow-x-scroll rounded-[4px] border-separate border-spacing-0">
         <thead className="bg-neutral50  leading-5 text-[#475467]">
-          <tr className="sm:text-[8px] lg:text-[16px] ">
+          <tr className="sm:text-[8px] lg:text-[14px] ">
             {tableHead.map((column, index) => (
               <th
                 key={column.key}
-                className={`p-[10px] lg:px-4 lg:py-3 text-left !font-medium whitespace-nowrap  ${
+                className={`p-[10px] lg:px-3 lg:py-3 text-left !font-medium whitespace-nowrap  ${
                   column.hiddenAbove1024 ? 'lg:hidden' : ''
                 } ${column.hiddenBelow1024 ? 'hidden lg:table-cell' : ''}`}
                 style={{
@@ -48,7 +48,7 @@ const ResponsiveTable: React.FC<ResponsiveTableProps> = ({
           className="bg-white divide-y divide-[#E4E7EC]"
           style={{ borderTop: 'unset' }}
         >
-          {tableData.map((row, rowIndex) => (
+          {tableData.map((row: any, rowIndex) => (
             <tr
               key={rowIndex}
               className="font-medium sm:text-10px lg:text-[14px] leading-5 text-left text-rgba-101828"
@@ -60,11 +60,24 @@ const ResponsiveTable: React.FC<ResponsiveTableProps> = ({
                     borderRadius: '4px'
                     // outline: "1px solid #E4E7EC",
                   }}
-                  className={`p-[9px] lg:p-4 whitespace-nowrap ${
+                  className={`p-[9px] lg:p-3 whitespace-nowrap ${
                     column.hiddenAbove1024 ? 'lg:hidden' : ''
                   } ${column.hiddenBelow1024 ? 'hidden lg:table-cell' : ''}`}
                 >
-                  {row[column.key] || '-'}
+                  {column.key === 'amount'
+                    ? row?.variants?.length > 0
+                      ? row?.variants[0]?.prices[0]?.amount
+                        ? `${
+                            row?.variants[0]?.prices[0]?.amount?.toFixed(2) ??
+                            ''
+                          }`
+                        : ''
+                      : row?.amount
+                      ? `${row?.amount?.toFixed(2) ?? ''}`
+                      : ''
+                    : typeof row[column.key] === 'string'
+                    ? row[column.key]
+                    : row[column.key]?.toString() || '-'}
                 </td>
               ))}
             </tr>
