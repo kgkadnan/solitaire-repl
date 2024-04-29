3cac82c31e188a07c617d1a12204f5b8e0bf8565
kgkadnan <adnan.badshah@kgkmail.com>
2024-04-29 12:32:28 +0530

    qc fix for bid to buy

--- src/app/v2/dashboard.tsx
+++ src/app/v2/dashboard.tsx
@@ -1583,7 +1583,7 @@ const Dashboard = () => {
                       {data.icon}{' '}
                     </div>
                     <div className="w-full">
-                      <div className="flex justify-between">
+                      <div className="flex justify-between items-baseline">
                         <p className="text-neutral600 text-mRegular">
                           {data.label}
                         </p>
