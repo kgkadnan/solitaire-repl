b3e2e529b168b5312dd7b472e0823961cb4e3edd
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-05-30 22:02:51 +0530

    improvised filter by stoneid

--- src/app/v2/new-arrivals/components/data-table/index.tsx
+++ src/app/v2/new-arrivals/components/data-table/index.tsx
@@ -147,7 +147,20 @@ const NewArrivalDataTable = ({
   const [paginatedData, setPaginatedData] = useState<any>([]);
   const [globalFilter, setGlobalFilter] = useState('');
   useEffect(() => {
-    console.log(rows, 'data', globalFilter);
+    if (globalFilter !== '') {
+      let data = rows.filter(
+        (data: any) => data?.lot_id?.startsWith(globalFilter)
+      );
+      const startIndex = pagination.pageIndex * pagination.pageSize;
+      const endIndex = startIndex + pagination.pageSize;
+      // Slice the data to get the current page's data
+      const newData = data.slice(startIndex, endIndex);
+      // Update the paginated data state
+      setPaginatedData(newData);
+    } else {
+      setPaginatedData(rows);
+    }
+    // console.log(rows, 'data', globalFilter,data);
   }, [globalFilter]);
   useEffect(() => {
     if (activeTab !== 2) {
@@ -406,10 +419,11 @@ const NewArrivalDataTable = ({
       columnOrder,
       rowSelection,
       isFullScreen: isFullScreen,
-      pagination
+      pagination,
+      globalFilter
     },
     positionToolbarAlertBanner: 'none',
-    enableFilters: true,
+    // enableFilters: true,
     enableColumnActions: false,
     enableDensityToggle: false,
     enableHiding: false,
@@ -431,6 +445,7 @@ const NewArrivalDataTable = ({
     onPaginationChange: setPagination, //hoist pagination state to your state when it changes internally
     manualFiltering: true,
     onGlobalFilterChange: setGlobalFilter,
+    // enableFilterMatchHighlighting:true,
     icons: {
       SearchIcon: () => (
         <Image src={searchIcon} alt={'searchIcon'} className="mr-[6px]" />
@@ -521,7 +536,7 @@ const NewArrivalDataTable = ({
 
     sortDescFirst: false,
     initialState: {
-      globalFilter,
+      // globalFilter,
       showGlobalFilter: true,
       expanded: true,
       grouping: ['shape'],
