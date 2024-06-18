95475071510e616aca8554a5eb4c29d583c011aa
kgkadnan <adnan.badshah@kgkmail.com>
2024-06-18 11:51:27 +0530

    fixes

--- src/utils/fix-two-digit-number.ts
+++ src/utils/fix-two-digit-number.ts
@@ -1,15 +1,20 @@
-export const formatNumber = (num: number) => {
-  let numStr = num?.toString();
+export const formatNumber = (num: number | null | undefined) => {
+  if (num == null) {
+    // This handles both null and undefined
+    return '-';
+  }
+
+  let numStr = num.toString();
 
   // Check if the number has a decimal point
-  if (!numStr?.includes('.')) {
+  if (!numStr.includes('.')) {
     numStr += '.00'; // If not, append ".00"
-  } else if (numStr?.split('.')[1].length === 1) {
+  } else if (numStr.split('.')[1].length === 1) {
     numStr += '0'; // If it has one decimal place, append a zero
   }
 
   // Use toFixed() to round the number to two decimal places
-  let formattedNum = parseFloat(numStr)?.toFixed(2);
+  let formattedNum = parseFloat(numStr).toFixed(2);
 
   return formattedNum;
 };
