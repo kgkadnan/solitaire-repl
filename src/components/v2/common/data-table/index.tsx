62393d1e9350804baf15783feb61c1d02ac03cab
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-06-26 12:55:43 +0530

    minor enhancement

--- src/components/v2/common/data-table/index.tsx
+++ src/components/v2/common/data-table/index.tsx
@@ -993,7 +993,9 @@ const DataTable = ({
                 selectedProducts={rowSelection}
                 setErrorText={setErrorText}
                 setIsError={setIsError}
-                shareTrackIdentifier={myCart ? 'Cart' : 'Search Results'}
+                shareTrackIdentifier={
+                  myCart ? 'Cart' : isDashboard ? 'Dashboard' : 'Search Results'
+                }
               />
             </div>
           </div>
