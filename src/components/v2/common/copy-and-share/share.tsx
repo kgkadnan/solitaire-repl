5130271ae62210eda6a858823f2ebe954365b067
kgkadnan <adnan.badshah@kgkmail.com>
2024-06-27 15:21:45 +0530

    fix share

--- src/components/v2/common/copy-and-share/share.tsx
+++ src/components/v2/common/copy-and-share/share.tsx
@@ -12,6 +12,7 @@ import { IProduct } from '@/app/v2/search/interface';
 import Tooltip from '../tooltip';
 import { formatNumber } from '@/utils/fix-two-digit-number';
 import { useLazyShareEventQuery } from '@/features/api/track-interaction';
+import { formatNumberWithCommas } from '@/utils/format-number-with-comma';
 
 const Share = ({
   rows,
@@ -164,8 +165,25 @@ const Share = ({
             }
             // Handle amount separately if it's selected
             if (attribute === 'amount' && selectedAttributes['amount']) {
-              const amount = product.amount || 0; // Or however you calculate amount
-              return `Amt ($): ${amount}`;
+              const amount = product?.variants[0]?.prices[0]?.amount
+                ? product?.variants[0]?.prices[0]?.amount
+                : product?.amount;
+              return `Amt ($): $${formatNumberWithCommas(amount)}`;
+            }
+            if (attribute === 'rap_value' && selectedAttributes['rap_value']) {
+              const rapValue = product.rap_value;
+              return `Rap Val ($): $${formatNumberWithCommas(rapValue)}`;
+            }
+            if (attribute === 'rap' && selectedAttributes['rap']) {
+              const rap = product.rap;
+              return `Rap ($): $${formatNumberWithCommas(rap)}`;
+            }
+            if (
+              attribute === 'price_per_carat' &&
+              selectedAttributes['price_per_carat']
+            ) {
+              const pricePerCarat = product.price_per_carat || 0;
+              return `Pr/Ct: $${formatNumberWithCommas(pricePerCarat)}`;
             }
             if (
               attribute === 'current_max_bid' &&
@@ -208,7 +226,7 @@ const Share = ({
             }
             // For other attributes, continue as before
             const option = shareOptions.find(opt => opt.state === attribute);
-            return option ? `${option.name}: ${product[attribute]}` : '';
+            return option ? `${option.name}: ${product[attribute] ?? '-'}` : '';
           })
           .filter(line => line) // Remove any undefined entries
           .join('\n');
