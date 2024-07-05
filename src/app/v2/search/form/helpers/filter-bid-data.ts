49db01926fab5264d3398d4e9056d32fc75ea839
kgkadnan <adnan.badshah@kgkmail.com>
2024-07-05 13:31:16 +0530

    fix for girdle search

--- src/app/v2/search/form/helpers/filter-bid-data.ts
+++ src/app/v2/search/form/helpers/filter-bid-data.ts
@@ -1,5 +1,17 @@
 export function filterBidData(data: any[], query: any): any[] {
   const skipKeys = ['key_to_symbol_search_type'];
+  const girdleOrder = [
+    'ETN',
+    'VTN',
+    'STN',
+    'THN',
+    'MED',
+    'STK',
+    'THK',
+    'VTK',
+    'ETK'
+  ];
+
   return data.filter(item => {
     // Check all properties from the query object
     for (const key in query) {
@@ -39,13 +51,26 @@ export function filterBidData(data: any[], query: any): any[] {
         }
       } else if (key.includes('[') && key.includes(']')) {
         const [mainKey, condition] = key.split(/\[|\]/).filter(Boolean);
-        const value = parseFloat(query[key] as string);
-        const itemValue = parseFloat(item[mainKey]);
+        const value = query[key] as string;
 
-        if (condition === 'lte' && !(itemValue <= value)) {
-          return false;
-        } else if (condition === 'gte' && !(itemValue >= value)) {
-          return false;
+        if (mainKey === 'girdle') {
+          const itemValue = item[mainKey] as string;
+          const itemIndex = girdleOrder.indexOf(itemValue);
+          const queryIndex = girdleOrder.indexOf(value);
+
+          if (condition === 'lte' && !(itemIndex <= queryIndex)) {
+            return false;
+          } else if (condition === 'gte' && !(itemIndex >= queryIndex)) {
+            return false;
+          }
+        } else {
+          const itemValue = parseFloat(item[mainKey]);
+
+          if (condition === 'lte' && !(itemValue <= parseFloat(value))) {
+            return false;
+          } else if (condition === 'gte' && !(itemValue >= parseFloat(value))) {
+            return false;
+          }
         }
       } else if (item[key] !== query[key]) {
         return false;
