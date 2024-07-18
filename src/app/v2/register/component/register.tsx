a11d29351b0003e1683baad5c2f2ac3d2558518b
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-07-18 18:31:49 +0530

    fixes

--- src/app/v2/register/component/register.tsx
+++ src/app/v2/register/component/register.tsx
@@ -66,7 +66,7 @@ const RegisterComponent = ({
         onSubmit={handleRegisterSubmit}
         className="flex items-center text-center"
       >
-        <div className="flex flex-col w-[450px] min-h-[880px] p-8 gap-[24px] rounded-[8px] border-[1px] border-neutral-200">
+        <div className="flex flex-col w-[450px] min-h-[880px] p-8 gap-[24px] rounded-[8px] border-[1px] border-neutral200 mb-[20px]">
           <div className="flex flex-col items-center">
             <Image src={KgkIcon} alt="KGKlogo" width={60} height={84} />
           </div>
