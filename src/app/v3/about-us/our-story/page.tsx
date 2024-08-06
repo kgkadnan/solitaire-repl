3a3120aec46680a3fcdb71e8bac0c13f74faefe5
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-08-06 13:09:27 +0530

    minor fix

--- src/app/v3/about-us/our-story/page.tsx
+++ src/app/v3/about-us/our-story/page.tsx
@@ -2,54 +2,21 @@
 
 import React, { useRef, useEffect, useState } from 'react';
 import Image from 'next/image';
-// import anime from 'animejs';
 import TimelineBanner from '@public/v3/timeline/timeline-banner.png';
 import { aboutUsFirstFold } from '@/constants/v3/about-us';
 import TimelineComponent from '@/components/v3/timeline';
 import classNames from 'classnames';
+import anime from 'animejs/lib/anime.es.js';
+import AnimatedTextDomino from '@/components/v3/animated-text/domino';
+import AnimatedTextCoffee from '@/components/v3/animated-text/coffee';
+import AnimatedTextFade from '@/components/v3/animated-text/fade';
+// import './style.module.scss'
 
 export default function AboutUs() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const sectionCount = aboutUsFirstFold.length;
-  // const [displayedText, setDisplayedText] = useState('');
-  // const [index, setIndex] = useState(0);
-  // const typewriterText = "KGK Diamonds: A Pillar of KGK Group";
-
-  // useEffect(() => {
-  //   if (index < typewriterText.length) {
-  //     const timeoutId = setTimeout(() => {
-  //       setDisplayedText(displayedText + typewriterText.charAt(index));
-  //       setIndex(index + 1);
-  //     }, 100); // Adjust the speed here
-  //     return () => clearTimeout(timeoutId);
-  //   }
-  // }, [index, displayedText, typewriterText]);
-
-  // useEffect(() => {
-  //   // Wrap every letter in a span for animation
-  //   const textWrapper = document.querySelector('.animated-text');
-  //   if (textWrapper) {
-  //     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-  //     anime.timeline({ loop: true })
-  //       .add({
-  //         targets: '.animated-text .letter',
-  //         scale: [4, 1],
-  //         opacity: [0, 1],
-  //         translateZ: 0,
-  //         easing: "easeOutExpo",
-  //         duration: 950,
-  //         delay: (el, i) => 70 * i
-  //       }).add({
-  //         targets: '.animated-text',
-  //         opacity: 0,
-  //         duration: 1000,
-  //         easing: "easeOutExpo",
-  //         delay: 1000
-  //       });
-  //   }
-  // }, [displayedText]);
+  const imageContainerRef = useRef<HTMLDivElement>(null);
 
   const scrollToIndex = (index: number) => {
     if (scrollContainerRef.current) {
@@ -108,13 +75,76 @@ export default function AboutUs() {
     };
   };
 
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observerCallback = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('timeline-animate-zoom');
+        } else {
+          entry.target.classList.remove('timeline-animate-zoom');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+    if (imageContainerRef.current) {
+      observer.observe(imageContainerRef.current);
+    }
+
+    return () => {
+      if (imageContainerRef.current) {
+        observer.unobserve(imageContainerRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    // Wrap every letter in a span
+    const textElements = document.querySelectorAll(
+      '.animated-text, .ml9 .letters'
+    );
+    textElements.forEach((textElement: any) => {
+      textElement.innerHTML = textElement.textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>"
+      );
+    });
+
+    // Anime.js animation
+    anime
+      .timeline({ loop: true })
+      .add({
+        targets: '.animated-text .letter, .ml9 .letter',
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+      })
+      .add({
+        targets: '.animated-text, .ml9',
+        opacity: 0,
+        duration: 1000,
+        easing: 'easeOutExpo',
+        delay: 1000
+      });
+  }, []);
+
   return (
     <div className="relative">
       <div className="min-h-[300px] flex items-center px-[112px] bg-animated-gradient bg-[length:200%_200%] bg-no-repeat animate-gradient">
         <div className="w-full">
           <div className="flex flex-col gap-14 pt-[180px] pb-[80px]">
-            <div className="text-neutral900 text-[108px] font-bold text-center leading-[100px] flowy-animate typewriter-text animated-text">
-              KGK Diamonds: A Pillar of KGK Group
+            <div className="text-neutral900 text-[108px] font-bold text-center leading-[100px]">
+              <AnimatedTextFade text="KGK Diamonds: A Pillar of KGK Group" />
             </div>
             {aboutUsFirstFold.map((aboutStep, index) => (
               <div
@@ -123,15 +153,15 @@ export default function AboutUs() {
                 key={index}
               >
                 <div className="flex gap-3">
-                  <p className="text-neutral900 text-[28px] font-bold w-1/2 animated-text">
-                    {aboutStep.subtitle}
+                  <p className="text-neutral900 text-[28px] font-bold w-1/2 ">
+                    <AnimatedTextCoffee text={aboutStep.subtitle} />
                   </p>
-                  <p className="text-neutral800 text-lRegular w-1/2 px-4 pt-[14px]">
-                    {aboutStep.description}
+                  <p className="text-neutral800 text-lRegular w-1/2 px-4 pt-[14px] ">
+                    <AnimatedTextFade text={aboutStep.description} />
                   </p>
                 </div>
                 <p className="text-neutral400 text-headingXL font-bold bottom-0 mt-[30px]">
-                  0{index + 1}
+                  <span>0{index + 1}</span>
                 </p>
               </div>
             ))}
@@ -140,23 +170,33 @@ export default function AboutUs() {
       </div>
       <div>
         <div className="relative">
-          <Image
-            src={TimelineBanner}
-            alt="Timeline banner"
-            className={classNames('w-full timeline-animate-zoom')}
-            style={{ clipPath: 'polygon(0 0, 100% 0, 100% 70%, 0 90%)' }}
-          />
+          <div ref={imageContainerRef}>
+            {/* <Image
+              src={TimelineBanner}
+              alt="Timeline banner"
+              className={classNames('w-full', 'timeline-animate-zoom')}
+              style={{ clipPath: 'polygon(0 0, 100% 0, 100% 70%, 0 90%)' }}
+            /> */}
+          </div>
           <div className="absolute top-[250px] px-[112px] text-neutral0 w-full flex justify-between">
             <div className="w-[500px]">
-              <p className="text-headingXL font-bold">The Timeline</p>
+              <p className="text-headingXL font-bold animated-text ml9">
+                <span className="text-wrapper">
+                  <span className="letters">The Timeline</span>
+                </span>
+              </p>
               <div className="flex gap-1">
                 <div className="absolute transform -translate-x-1/2 top-20 h-full">
                   <div className="h-[75px] mr-4 w-[2px] bg-[white] mx-auto"></div>
                 </div>
-                <p className="text-headingS">
-                  KGK Group, founded by Shri Kesrimal Ji of Jaipur in 1905,
-                  began with a humble task of trading coloured gemstones from
-                  India.
+                <p className="text-headingS animated-text ml9">
+                  <span className="text-wrapper">
+                    <span className="letters">
+                      KGK Group, founded by Shri Kesrimal Ji of Jaipur in 1905,
+                      began with a humble task of trading coloured gemstones
+                      from India.
+                    </span>
+                  </span>
                 </p>
               </div>
             </div>
--- src/app/v3/page.tsx
+++ src/app/v3/page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // import dynamic from 'next/dynamic';
 // import GlobeComponent from '@/components/v3/globe';
 import AnimatedDotMap from '@/components/v3/animated-map';
+import AnimatedTextDomino from '@/components/v3/animated-text/domino';
+import AnimatedTextCoffee from '@/components/v3/animated-text/coffee';
 
 // const WorldMap = dynamic(() => import('@/components/v3/world-map'), {
 //   ssr: false
@@ -11,6 +13,12 @@ const index = () => {
   return (
     <div>
       <AnimatedDotMap />
+      <div className="flex items-center justify-center h-screen">
+        <AnimatedTextDomino text={'hello'} />
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <AnimatedTextCoffee text={'hello'} />
+      </div>
       {/* <WorldMap /> */}
       {/* <GlobeComponent /> */}
     </div>
