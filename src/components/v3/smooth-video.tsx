8208a0a2d8f26e759cf66f3a5a9a22ed4114fd57
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-08-20 11:14:11 +0530

    smooth scroll animation added

--- src/components/v3/smooth-video.tsx
+++ src/components/v3/smooth-video.tsx
@@ -1,10 +1,24 @@
 'use client';
 
 import React, { useRef, useState, useEffect } from 'react';
+import Pause from '@public/v3/icons/pause.svg';
+import Play from '@public/v3/icons/play.svg';
+
+import Image from 'next/image';
 
 const SmoothVideoPlayer = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef: any = useRef<HTMLVideoElement>(null);
   const [scrollPosition, setScrollPosition] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -13,26 +27,15 @@ const SmoothVideoPlayer = () => {
 
       const rect = videoElement.getBoundingClientRect();
       const windowHeight = window.innerHeight;
-      // console.log(rect.top)
-      // if (rect.top >= windowHeight) {
-      //   setScrollPosition(1); // Video is fully visible, no inset
-      // } else
-      if (Math.abs(rect.top) < windowHeight) {
-        // Calculate how far rect.top has moved up (negative value)
-        let visibleRatio;
-        if (rect.top < 0) {
-          const negativeTop = Math.abs(rect.top);
 
-          // Calculate the visible ratio based on how far out of view the top is
-          visibleRatio = Math.max(1 - negativeTop / windowHeight, 0);
-        } else {
-          const negativeTop = Math.abs(rect.top);
-
-          // Calculate the visible ratio based on how far out of view the top is
-          visibleRatio = Math.max(1 + negativeTop / windowHeight, 0);
-        }
-
-        setScrollPosition(visibleRatio);
+      if (rect.top < 0 && Math.abs(rect.top) < windowHeight) {
+        const negativeTop = Math.abs(rect.top);
+        const visibleRatio = Math.max(1 - negativeTop / windowHeight, 0);
+        visibleRatio <= 0.8 && setScrollPosition(visibleRatio);
+      } else if (rect.top < windowHeight / 2) {
+        setScrollPosition(0.8);
+      } else {
+        setScrollPosition(1);
       }
     };
 
@@ -42,34 +45,40 @@ const SmoothVideoPlayer = () => {
     };
   }, []);
 
-  // Clip-path logic without any transform/zoom effect
   const clipPathValue =
     scrollPosition === 1
-      ? 'inset(0% round 44px)' // Fully visible
-      : `inset(${(1 - scrollPosition) * 6.25}% round 44px)`; // Increase inset as video moves out
-  console.log(scrollPosition);
+      ? 'inset(0% round 0px)' // Fully visible with no border-radius
+      : `inset(${(1 - scrollPosition) * 6.25}% round ${
+          44 * (1 - scrollPosition)
+        }px)`; // Increase inset and border-radius as video moves out
+
+  const translateX = scrollPosition === 1 ? 0 : -59.3359 * (1 - scrollPosition);
+  const translateY = scrollPosition === 1 ? 0 : -25 * (1 - scrollPosition);
+
   return (
     <div
-      className="welcome-video-wall-container h-[700px]"
+      className="welcome-video-wall-container h-[700px] relative"
       style={{
         clipPath: clipPathValue,
         transition: 'clip-path 0.5s ease'
       }}
     >
       <div
-        className="welcome-video-content-container"
+        className="welcome-video-content-container "
         style={{
-          transition: 'transform 0.5s ease' // Apply smooth transition for clip-path changes
+          transition: 'transform 0.5s ease',
+          position: 'relative'
         }}
       >
         <video
           ref={videoRef}
-          className="welcome-video-video"
+          className="welcome-video-video "
           muted
           playsInline
           loop
           preload="auto"
           role="img"
+          autoPlay
           aria-label="A video showcasing the lineup of Mac products"
           src="/v3/videos/globe.mp4"
           style={{
@@ -77,6 +86,22 @@ const SmoothVideoPlayer = () => {
             height: 'auto'
           }}
         ></video>
+      </div>
+      <div
+        className="absolute bottom-10 right-20 cursor-pointer"
+        style={{
+          // bottom: '30px',
+          // right: '30px',
+          transform: `matrix(1, 0, 0, 1, ${translateX}, ${translateY})`,
+          opacity: 0.817312,
+          transition: 'transform 0.5s ease'
+        }}
+      >
+        <Image
+          src={isPlaying ? Pause : Play}
+          onClick={handlePlayPause}
+          alt="video control"
+        />
       </div>
     </div>
   );
