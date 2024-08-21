7e4b707eb7b3b622771c02ee4443054bf39b54ff
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-08-21 10:37:21 +0530

    quotes removed

--- src/constants/v3/sustainability.ts
+++ src/constants/v3/sustainability.ts
@@ -48,7 +48,7 @@ export const sustainabilitySection = [
     id: '02',
     header: 'Enhancing Food Security and Sustainability',
     imageTitle: 'Zero <br/>  Hunger',
-    description: `“KGK's kitchen garden initiative improves food security in Botswana by supplying fresh greens and reducing food import bills. Feeding over 500 people daily, KGK ensures employees receive nutritious meals. Additionally, a zero-waste culture in the canteen minimizes food waste, benefiting both the environment and the economy.”`,
+    description: `KGK's kitchen garden initiative improves food security in Botswana by supplying fresh greens and reducing food import bills. Feeding over 500 people daily, KGK ensures employees receive nutritious meals. Additionally, a zero-waste culture in the canteen minimizes food waste, benefiting both the environment and the economy.`,
     images: Sus2,
     head: Head2,
     color: '#E8F5E9'
@@ -57,7 +57,7 @@ export const sustainabilitySection = [
     id: '03',
     header: 'Bhagwan Mahaveer Cancer Hospital and Research Centre',
     imageTitle: 'Good Health <br/>& Well-Being',
-    description: `“Established in 1997 and funded by the KGK Group, the Bhagwan Mahaveer Cancer Hospital and Research Centre (BMCHRC) in Jaipur is North India's only super specialty cancer treatment hospital. The 300-bed, NABH-accredited institution offers advanced medical services, focusing on compassionate and humane patient care. With departments in surgical, medical, and radiation oncology, and an all-women’s Cancer Care wing, BMCHRC provides comprehensive cancer treatment and support, including moral and financial aid to the disadvantaged.”`,
+    description: `Established in 1997 and funded by the KGK Group, the Bhagwan Mahaveer Cancer Hospital and Research Centre (BMCHRC) in Jaipur is North India's only super specialty cancer treatment hospital. The 300-bed, NABH-accredited institution offers advanced medical services, focusing on compassionate and humane patient care. With departments in surgical, medical, and radiation oncology, and an all-women’s Cancer Care wing, BMCHRC provides comprehensive cancer treatment and support, including moral and financial aid to the disadvantaged.`,
     images: Sus3,
     head: Head3,
     color: '#F0F0F0'
@@ -66,7 +66,7 @@ export const sustainabilitySection = [
     id: '04',
     header: 'Fostering Quality Education',
     imageTitle: 'Quality <br/> Education',
-    description: `“The KGK Group is a key supporter of the non-profit SS Jain Subodh Shiksha Samiti, which educates over 33,000 students in Rajasthan, India. Founded in 1918 with just four girls, it now encompasses 19 institutions, including playgroups, colleges, and professional institutes. Guided by the principle that quality education is an undeniable right, the Samiti's 900-member team is dedicated to ensuring all students can reach their full potential.” “KGK Academy enhances workplace skills through its TVET institute, promoting quality education and professional growth. Additionally, KGK Kids provides creche and daycare services, supporting early childhood development and enabling parents to focus on their careers.”`,
+    description: `The KGK Group is a key supporter of the non-profit SS Jain Subodh Shiksha Samiti, which educates over 33,000 students in Rajasthan, India. Founded in 1918 with just four girls, it now encompasses 19 institutions, including playgroups, colleges, and professional institutes. Guided by the principle that quality education is an undeniable right, the Samiti's 900-member team is dedicated to ensuring all students can reach their full potential.” “KGK Academy enhances workplace skills through its TVET institute, promoting quality education and professional growth. Additionally, KGK Kids provides creche and daycare services, supporting early childhood development and enabling parents to focus on their careers.`,
     images: Sus4,
     head: Head4,
     color: '#E0F7FA'
@@ -80,7 +80,7 @@ export const sustainabilitySection = [
     imageTitle: 'Gender <br/> Equality',
     description: [
       `The Khilti Pari Foundation focuses on empowering underprivileged women in India by providing education, health awareness, and vocational training. Programs like "Pad-for-Pad" educate girls on menstrual hygiene, while scholarship and tuition support ensure continued education. By addressing gender inequality and breaking the cycle of child marriage, the foundation helps women achieve their potential.`,
-      `“KGK Diamonds champions gender equality with a balanced workforce comprising 49% males and 51% females. This commitment to equal employment opportunities fosters a diverse and inclusive work environment.”`
+      `KGK Diamonds champions gender equality with a balanced workforce comprising 49% males and 51% females. This commitment to equal employment opportunities fosters a diverse and inclusive work environment.`
     ],
     images: Sus5,
     head: Head5,
@@ -90,7 +90,7 @@ export const sustainabilitySection = [
     id: '06',
     header: 'Sustainable Water Management Practices',
     imageTitle: 'Clean Water <br/> & Sanitation',
-    description: `“KGK Botswana promotes water sustainability by harvesting rainwater for irrigating its kitchen garden. Additionally, the facility recycles all water daily, ensuring efficient and eco-friendly water usage.”`,
+    description: `KGK Botswana promotes water sustainability by harvesting rainwater for irrigating its kitchen garden. Additionally, the facility recycles all water daily, ensuring efficient and eco-friendly water usage.`,
     images: Sus6,
     head: Head6,
     color: '#E8F5E9'
@@ -99,7 +99,7 @@ export const sustainabilitySection = [
     id: '07',
     header: 'Harnessing Solar Energy',
     imageTitle: 'Affordable &  <br/> Clean Energy',
-    description: `“KGK Botswana has installed a 462 kWh solar rooftop, generating the majority of the factory's green energy needs. This initiative supports sustainable energy use and reduces the carbon footprint, aligning with global goals for affordable and clean energy.”`,
+    description: `KGK Botswana has installed a 462 kWh solar rooftop, generating the majority of the factory's green energy needs. This initiative supports sustainable energy use and reduces the carbon footprint, aligning with global goals for affordable and clean energy.`,
     images: Sus7,
     head: Head7,
     color: '#E0F7FA'
@@ -128,7 +128,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '10',
     header: 'Reducing Inequalities through Inclusive Employment',
     imageTitle: 'Reduce <br/> Inequalities',
-    description: `“KGK promotes youth in Botswana to supervisory roles and ensures equal opportunities for all employees, reducing inequalities. The inclusive workforce includes members of the deaf community, demonstrating KGK's commitment to diversity and equal opportunity.”`,
+    description: `KGK promotes youth in Botswana to supervisory roles and ensures equal opportunities for all employees, reducing inequalities. The inclusive workforce includes members of the deaf community, demonstrating KGK's commitment to diversity and equal opportunity.`,
     images: Sus10,
     head: Head10,
     color: '#E0E0E0'
@@ -137,7 +137,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '11',
     header: 'Jal Mahal reconstruction',
     imageTitle: 'Sustainable <br/> Cities & Communities',
-    description: `"One of the most picturesque monuments in Jaipur is the enchanting Jal Mahal Palace. ...In 2005, the restoration began... The KGK Group has been instrumental in restoring the palace to its old glory. ... Given the historical importance of the place, every little detail was studied carefully and restored keeping its original beauty intact."`,
+    description: `One of the most picturesque monuments in Jaipur is the enchanting Jal Mahal Palace. ...In 2005, the restoration began... The KGK Group has been instrumental in restoring the palace to its old glory. ... Given the historical importance of the place, every little detail was studied carefully and restored keeping its original beauty intact.`,
     images: Sus11,
     head: Head11,
     color: '#E0E0E0'
@@ -146,7 +146,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '12',
     header: 'Championing Responsible Consumption and Production',
     imageTitle: 'Responsible <br/> Consumption & Production',
-    description: `“KGK Diamonds Botswana utilizes green energy, harvests rainwater, and employs drip irrigation for its garden. They segregate solid waste, recycle kitchen waste into compost, and promote a zero-waste culture, enhancing environmental sustainability and food security.”`,
+    description: `KGK Diamonds Botswana utilizes green energy, harvests rainwater, and employs drip irrigation for its garden. They segregate solid waste, recycle kitchen waste into compost, and promote a zero-waste culture, enhancing environmental sustainability and food security.`,
     images: Sus12,
     head: Head12,
     color: '#E8F5E9'
@@ -155,7 +155,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '13',
     header: 'Committing to Climate Action',
     imageTitle: 'Climate <br/> Action',
-    description: `“KGK reduces Scope 2 emissions by 30% through green energy use. Rainwater harvesting and grey water recycling save 20 million liters of water annually. Solid waste segregation and composting minimize landfill dependence and emissions, supporting climate action goals.”`,
+    description: `KGK reduces Scope 2 emissions by 30% through green energy use. Rainwater harvesting and grey water recycling save 20 million liters of water annually. Solid waste segregation and composting minimize landfill dependence and emissions, supporting climate action goals.`,
     images: Sus13,
     head: Head13,
     color: '#E0E0E0'
@@ -164,7 +164,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '15',
     header: 'Balancing Nature and Business',
     imageTitle: 'Life <br/> on Land',
-    description: `“KGK’s gardens provide ample oxygen and fresh air for factory workers, promoting a healthy environment. This initiative ensures a harmonious balance between nature and business operations.”`,
+    description: `KGK’s gardens provide ample oxygen and fresh air for factory workers, promoting a healthy environment. This initiative ensures a harmonious balance between nature and business operations.`,
     images: Sus15,
     head: Head15,
     color: '#E8F5E9'
@@ -173,7 +173,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '16',
     header: 'Ensuring Peace, Justice, and Strong Institutions',
     imageTitle: 'Peace, Justice <br/> & Strong Institutions',
-    description: `“KGK upholds high compliance standards and undergoes rigorous audits, including Best Practice Principles, Forevermark Pipeline Integrity, Responsible Jewelry Council, and Tiffany Compliance audits. These practices ensure integrity and accountability within the industry.”`,
+    description: `KGK upholds high compliance standards and undergoes rigorous audits, including Best Practice Principles, Forevermark Pipeline Integrity, Responsible Jewelry Council, and Tiffany Compliance audits. These practices ensure integrity and accountability within the industry.`,
     images: Sus16,
     head: Head16,
     color: '#F4F4F4'
@@ -182,7 +182,7 @@ Sophisticated Technological Investments: KGK has invested in several sophisticat
     id: '17',
     header: 'Building Partnerships for Sustainable Development',
     imageTitle: 'Partnerships <br/> for the Goals',
-    description: `“KGK collaborates with various institutions, including Botho University, Botswana Society for the Deaf, and the Ministry of Minerals & Energy, to support education, employment, and community empowerment. These partnerships enhance social, economic, and environmental sustainability, contributing to global development goals.”`,
+    description: `KGK collaborates with various institutions, including Botho University, Botswana Society for the Deaf, and the Ministry of Minerals & Energy, to support education, employment, and community empowerment. These partnerships enhance social, economic, and environmental sustainability, contributing to global development goals.`,
     images: Sus17,
     head: Head17,
     color: '#DDEFF2'
