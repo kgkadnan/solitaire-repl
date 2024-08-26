4641ea28b2462ce100c698a00af7d77b3e02def7
kgkadnan <adnan.badshah@kgkmail.com>
2024-08-26 17:07:05 +0530

    fix loader

--- src/components/v2/common/input-field/dynamic-mobile/index.tsx
+++ src/components/v2/common/input-field/dynamic-mobile/index.tsx
@@ -92,7 +92,11 @@ export const DynamicMobileInput = ({
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
-    !isNotEditable && setIsOpen(!isOpen);
+    if (!isNotEditable) {
+      if (!isFlagLoaded) {
+        setIsOpen(!isOpen);
+      }
+    }
   };
 
   const customFilter = (option: any, inputValue: any) => {
@@ -126,7 +130,7 @@ export const DynamicMobileInput = ({
               <div className="relative flex justify-center items-center">
                 {isFlagLoaded && (
                   <div
-                    className="absolute flex justify-center items-center bg-neutral0 w-[100px] h-[21px] top-[1px]"
+                    className="absolute flex justify-center items-center bg-neutral0 rounded-[4px] w-[105px] top-[-7px] h-[37px]"
                     onClick={e => {
                       e.stopPropagation();
                     }}
