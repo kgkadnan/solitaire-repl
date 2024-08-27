a7687017ec7c7b8853b7045f94644c7494a30df8
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-08-27 17:56:54 +0530

    globe variation

--- src/app/v3/test1/page.tsx
+++ src/app/v3/test1/page.tsx
@@ -1,13 +1,13 @@
-// import VideoScrollComponentNew from '@/components/v3/dummy';
+import VideoScrollComponentNew from '@/components/v3/dummy';
 // import LandingPage from '@/components/v3/dummy';
-import VideoScrollComponent from '@/components/v3/video-scroll';
+// import VideoScrollComponent from '@/components/v3/video-scroll';
 import React from 'react';
 
 const page = () => {
   return (
     <div className="flex justify-center top-150">
-      <VideoScrollComponent />
-      {/* <VideoScrollComponentNew /> */}
+      {/* <VideoScrollComponent /> */}
+      <VideoScrollComponentNew />
       {/* <LandingPage/> */}
     </div>
   );
