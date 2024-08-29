e90b493275d2ffcf773887c9e05695e7d5ceadac
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-08-29 19:00:36 +0530

    transition smoothen

--- src/components/v3/traceability-html.tsx
+++ src/components/v3/traceability-html.tsx
@@ -76,7 +76,7 @@ const TraceabilityHtml = () => {
         Your browser does not support the video tag.
       </video>
       <div className="absolute bottom-8 flex justify-between space-x-4 bg-[#FFFFFF24] border-[white] rounded-[8px] px-[24px] py-[12px] border-[1px] min-w-[200px]">
-        <div className="flex space-x-2 items-center ">
+        <div className="flex space-x-2 items-center">
           {[
             { timeStart: 0, timeEnd: 3 },
             { timeStart: 3, timeEnd: 11 },
@@ -87,17 +87,23 @@ const TraceabilityHtml = () => {
             <div
               key={`${timeStart}-${timeEnd}`}
               onClick={() => handleReferenceClick(timeStart)}
-              className={` h-2 rounded-[8px] rounded-full cursor-pointer ${dotClasses(
+              className={`relative h-2 rounded-[8px] cursor-pointer transition-all duration-300 ease-in-out overflow-hidden ${dotClasses(
                 timeStart,
                 timeEnd
               )}`}
             >
-              <div
-                className="absolute top-0 left-0 h-full bg-neutral700 rounded-[16px]"
-                style={{
-                  width: calculateProgressWidth(timeStart, timeEnd, currentTime)
-                }}
-              />
+              {currentTime > timeStart && currentTime <= timeEnd && (
+                <div
+                  className="absolute top-0 left-0 h-full bg-neutral700 transition-width duration-300 ease-in-out rounded-[12px]"
+                  style={{
+                    width: calculateProgressWidth(
+                      timeStart,
+                      timeEnd,
+                      currentTime
+                    )
+                  }}
+                />
+              )}
             </div>
           ))}
         </div>
@@ -121,85 +127,6 @@ const TraceabilityHtml = () => {
     </div>
   );
 };
-
-// const RightStructure = ({ currentTime }: any) => {
-//   return (
-//     <>
-//       {traceabilityData.map(
-//         (data, index) =>
-//           currentTime > data.timeStart &&
-//           (index === 4 ? currentTime <= 31 : currentTime <= data.timeEnd) && (
-//             <div
-//               key={data.header1}
-//               className={`w-[420px] bg-[#FFFFFF57] p-[20px] flex flex-col rounded-[12px] gap-1 transition-opacity duration-500 z-9999`}
-//               style={{ boxShadow: 'var(--popups-shadow' }}
-//             >
-//               <p className="text-[16px] text-neutral900">Diamond Journey</p>
-//               <div className="rounded-[8px] bg-[#ffffff] p-[12px] flex flex-col gap-1">
-//                 <div className="flex gap-2">
-//                   <Image
-//                     src={data.icon}
-//                     alt="trace steps"
-//                     className="w-[40px] h-[40px]"
-//                   />
-//                   <p className="text-[16px] text-neutral900 font-semiBold">
-//                     {data.header1}
-//                   </p>
-//                 </div>
-//                 <div className="flex gap-2">
-//                   <div className="min-w-[40px] pl-[20px]">
-//                     <div className="border-l border-dotted border-gray-400 h-[120%] mt-[-20px]"></div>
-//                   </div>
-//                   {data.timeStart !== 0 ? (
-//                     <ol className="list-disc pl-[20px]">
-//                       {data.data.map((list: string, index: number) => (
-//                         <li key={`list-${index}`}>{list}</li>
-//                       ))}
-//                     </ol>
-//                   ) : (
-//                     <div className="flex flex-col gap-2">
-//                       <ol className="list-disc pl-[20px]">
-//                         <li key={`list-${index}`}>{data.data[0]}</li>
-//                         <li key={`list-${index}`}>{data.data[1]}</li>
-//                       </ol>
-//                       <p className="font-semiBold text-[12px]">
-//                         {' '}
-//                         Splitting & Barcoding
-//                       </p>
-//                       <ol className="list-disc pl-[20px]">
-//                         <li key={`list-${index}`}>{data.data[2]}</li>
-//                         <li key={`list-${index}`}>{data.data[3]}</li>
-//                       </ol>
-//                     </div>
-//                   )}
-//                 </div>
-//                 <div className="flex gap-2 items-center">
-//                   <Image
-//                     src={TraceEnd}
-//                     alt="trace steps"
-//                     className="w-[40px] h-[40px]"
-//                   />
-//                   <p className="text-[16px] text-neutral900 font-semiBold">
-//                     {data.header2}
-//                   </p>
-//                 </div>
-//                 <div className="pl-[48px] flex gap-2 flex-wrap">
-//                   {data.tags.map((tag: string, index: number) => (
-//                     <div
-//                       className="bg-[#E4E7EC] rounded-[4px] px-[6px] py-[8px] text-neutral800 text-[12px]"
-//                       key={`${tag}-${index}`}
-//                     >
-//                       {tag}
-//                     </div>
-//                   ))}
-//                 </div>
-//               </div>
-//             </div>
-//           )
-//       )}
-//     </>
-//   );
-// };
 
 const RightStructure = ({ currentTime }: any) => {
   const [activeData, setActiveData] = useState<any>(null);
