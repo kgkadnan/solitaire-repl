cb755775d82470c3384644090965193085161b98
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-04 12:09:58 +0530

    build fixes

--- src/components/v3/section-3d/html.tsx
+++ src/components/v3/section-3d/html.tsx
@@ -1,3 +1,279 @@
+// import React, { useEffect, useState } from 'react';
+// import Image from 'next/image';
+// import ExploreNow from '@public/v3/home/explore.svg';
+// import { useRouter } from 'next/navigation';
+// import US from '@public/v3/home/map/usa.svg';
+// import India from '@public/v3/home/map/india.svg';
+// import Belgium from '@public/v3/home/map/belgium.svg';
+// import Dubai from '@public/v3/home/map/dubai.svg';
+// import Hongkong from '@public/v3/home/map/hongkong.png';
+
+// // import ShimmerButton from '../animated-button';
+
+// const HtmlAnimation = () => {
+//   const [phoneVisible, setPhoneVisible] = useState(false);
+//   const [showBanner, setShowBanner] = useState(false);
+//   const [scrollIndex, setScrollIndex] = useState(0);
+//   const router = useRouter();
+
+//   const imageList = [
+//     '/v3/home/usa.png',
+//     '/v3/home/india.png',
+//     '/v3/home/belgium.png',
+//     '/v3/home/hongkong.png',
+//     '/v3/home/dubai.png'
+//   ];
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//       });
+//     }, 0);
+//   }, []);
+
+//   useEffect(() => {
+//     const phoneTimer = setTimeout(() => {
+//       setPhoneVisible(true);
+//     }, 300); // Delay before the phone and cards start scrolling up
+
+//     const bannerTimer = setTimeout(() => {
+//       setShowBanner(true);
+//     }, 1500); // Delay to show the banner after the phone and cards scroll up
+
+//     return () => {
+//       clearTimeout(phoneTimer);
+//       clearTimeout(bannerTimer);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     if (showBanner) {
+//       const scrollTimer = setInterval(() => {
+//         setScrollIndex(prevIndex => {
+//           if (prevIndex < imageList.length - 1) {
+//             return prevIndex + 1;
+//           } else {
+//             clearInterval(scrollTimer);
+//             return prevIndex;
+//           }
+//         });
+//       }, 2000); // Time between each card scroll
+//       return () => clearInterval(scrollTimer);
+//     }
+//   }, [showBanner, imageList.length]);
+
+//   const leftContainer = [
+//     {
+//       name: 'U.S.',
+//       image: US
+//     },
+//     {
+//       name: 'INDIA',
+//       image: India
+//     },
+//     {
+//       name: 'BELGIUM',
+//       image: Belgium
+//     },
+//     {
+//       name: 'UAE',
+//       image: Dubai
+//     },
+//     {
+//       name: 'HONG KONG',
+//       image: Hongkong
+//     }
+//   ];
+
+//   return (
+//     <div className="relative w-full" style={{ height: 'calc(100vh - 80px)' }}>
+//       <div className="flex justify-between relative xl:px-[112px] lg:px-[32px]">
+//         {/* Left div */}
+//         <div
+//           className={`flex flex-col gap-4  w-[250px] transition-opacity duration-700 ease-in-out transform ${
+//             showBanner
+//               ? 'opacity-100 scale-100 translate-x-0'
+//               : 'opacity-0 scale-90 translate-x-1/2'
+//           }`}
+//           style={{
+//             marginBottom:
+//               window && window?.innerWidth >= 1600
+//                 ? '45%'
+//                 : window?.innerWidth >= 1280
+//                 ? '65%'
+//                 : '75%',
+//             marginTop: 'auto'
+//           }}
+//         >
+//           <p className="text-[32px] font-semiBold text-neutral900 leading-[36px]">
+//             KGK Inventory Locations
+//           </p>
+//           <div className="flex gap-4 flex-wrap">
+//             {leftContainer.map(data => (
+//               <div
+//                 key={data.name}
+//                 className="w-fit flex justify-center flex-col items-center"
+//               >
+//                 <div className="w-[44px] h-[44px]">
+//                   <Image src={data.image} alt={data.name} />
+//                 </div>
+//                 <p className="text-[20px] text-neutral900">{data.name}</p>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//         {/* // phoneVisible
+//             //   ? 'translate-y-[-210px] 2xl:translate-y-[-210px] lg:translate-y-[-290px] 1600px:translate-y-[-180px] 1920px:translate-y-[-180px]'
+//             //   : // ? ' lg:translate-y-[-262px]  xl:translate-y-[-200px] 2xl:translate-y-[-200px] 3xl:translate-y-[-60px]'
+//                 // 'translate-y-[calc(1200px - 100vh)]'
+//           // } */}
+//         {/* Phone Skeleton */}
+//         <div
+//           className={`relative w-[460px] h-[1200px] bg-no-repeat bg-contain transition-transform duration-700 ease-in-out z-10  flex justify-center`}
+//           style={{
+//             backgroundImage: `url('/v3/home/phone-skeleton.png')`,
+//             zIndex: '10 !important', // Ensure phone skeleton is on top
+//             // transform: `${
+//             //   phoneVisible
+//             //     ? 'translateY(calc(100vh - 1000px))'
+//             //     : 'translateY(100vh)'
+//             // }`
+//             transform:
+//               phoneVisible && window
+//                 ? window?.innerWidth >= 1280 // xl breakpoint (1280px and above)
+//                   ? 'translateY(calc(100vh - 950px))'
+//                   : window?.innerWidth >= 1024 // lg breakpoint (1024px and above)
+//                   ? 'translateY(calc(100vh - 895px))'
+//                   : 'translateY(calc(100vh - 800px))' // Default for smaller screens
+//                 : 'translateY(100vh)'
+//           }}
+//         >
+//           <div
+//             className={`absolute  transform -translate-x-1/2 w-[370px] h-[700px] overflow-hidden transition-transform duration-700 ease-in-out z-0 flex flex-col items-center`}
+//             style={{
+//               zIndex: -1, // Ensure cards are beneath the phone skeleton
+
+//               transform: phoneVisible
+//                 ? //   ? window.innerWidth >= 1280 // xl breakpoint (1280px and above)
+//                   //     ? 'translateY(calc(100vh - 350px))'
+//                   //     : window.innerWidth >= 1024 // lg breakpoint (1024px and above)
+//                   //     ? 'translateY(calc(100vh - 250px))'
+//                   //     : 'translateY(calc(100vh - 410px))' // Default for smaller screens
+//                   //   :
+//                   'translateY(385px)'
+//                 : 'translateY(100vh)'
+//             }}
+//           >
+//             <ul
+//               className="list-none p-0 transition-transform duration-700 ease-in-out"
+//               style={{
+//                 transform: `translateY(-${
+//                   scrollIndex * (100 / imageList.length)
+//                 }%)` // Adjust scroll position
+//               }}
+//             >
+//               {imageList.map((src, index) => (
+//                 <li key={index}>
+//                   <img
+//                     src={src}
+//                     alt={`Slide ${index + 1}`}
+//                     className={`w-full mx-auto py-[6px] ${
+//                       scrollIndex !== 0 && ''
+//                     }`}
+//                   />
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         </div>
+
+//         {/* Scrolling Cards */}
+//         {/* lg:top-[120px] xl:top-[180px] 2xl:top-[180px] 3xl:top-[330px]  */}
+//         {/* ${
+//             phoneVisible ? 'translate-y-[120px]' : 'translate-y-full'
+//           } */}
+
+//         {/* Right div */}
+//         <div
+//           className={`text-neutral900 w-[250px] transition-opacity duration-700 ease-in-out transform ${
+//             showBanner
+//               ? 'opacity-100 scale-100 translate-x-0'
+//               : 'opacity-0 scale-90 translate-x-1/2'
+//           }`}
+//           style={{
+//             marginBottom:
+//               window && window?.innerWidth >= 1600
+//                 ? '45%'
+//                 : window?.innerWidth >= 1280
+//                 ? '65%'
+//                 : '75%',
+
+//             marginTop: 'auto'
+//           }}
+//         >
+//           <div className=" flex flex-col  text-neutral900">
+//             {/* <span className="text-[48px] font-semiBold">2.0</span> */}
+//             {/* <span className="text-[20px]">
+//               Points
+//               <br /> below
+//             </span>{' '} */}
+//             <span
+//               className="text-[48px] font-semiBold w-fit"
+//               style={{
+//                 background: 'linear-gradient(90deg, #DBF2FC, #E8E8FF, #FFF4E3)',
+//                 padding: '4px 8px',
+//                 borderRadius: '4px',
+//                 backgroundColor: 'transparent'
+//               }}
+//             >
+//               50K+
+//             </span>
+//             <p className="text-[20px] text-neutral900 pl-2">
+//               Stones in Inventory
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* Banner that appears after the phone and cards scroll up */}
+//       <div
+//         className={`xl:h-[240px] lg:h-[190px] absolute bottom-0 w-full text-center bg-white flex justify-center shadow-lg transition-transform duration-700 ease-in-out ${
+//           showBanner ? 'translate-y-0' : 'translate-y-full'
+//         }`}
+//         style={{
+//           // height: '280px',
+//           zIndex: 10,
+//           boxShadow: '0 4px 20px rgba(44, 110, 110, 0.38)'
+//         }}
+//       >
+//         <div className="bg-white pt-5 h-full relative flex flex-col justify-center items-center">
+//           <h1 className="xl:text-[52px] lg:text-[42px] text-neutral900 font-black leading-[48px]">
+//             Access KGK’s Global Diamond Inventory
+//           </h1>
+//           <p className="xl:text-[36px] lg:text-[28px] text-neutral900  mb-[20px] font-semiBold">
+//             Create a Guest Account
+//           </p>
+//           <Image
+//             src={ExploreNow}
+//             alt="explore now"
+//             onClick={() => router.push('/v2/register')}
+//             className="cursor-pointer lg:w-[224px]"
+//           />
+//           {/* <ShimmerButton
+//             className="!rounded-[8px] w-[200px] h-[44px]"
+//             onClick={() => router.push('/v2/register')}
+//           >
+//             Explore Now!
+//           </ShimmerButton> */}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HtmlAnimation;
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import ExploreNow from '@public/v3/home/explore.svg';
@@ -8,12 +284,11 @@ import Belgium from '@public/v3/home/map/belgium.svg';
 import Dubai from '@public/v3/home/map/dubai.svg';
 import Hongkong from '@public/v3/home/map/hongkong.png';
 
-// import ShimmerButton from '../animated-button';
-
 const HtmlAnimation = () => {
   const [phoneVisible, setPhoneVisible] = useState(false);
   const [showBanner, setShowBanner] = useState(false);
   const [scrollIndex, setScrollIndex] = useState(0);
+  const [windowWidth, setWindowWidth] = useState<any>(); // Track window width
   const router = useRouter();
 
   const imageList = [
@@ -25,22 +300,25 @@ const HtmlAnimation = () => {
   ];
 
   useEffect(() => {
-    setTimeout(() => {
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window?.innerWidth || 0); // Set window width on mount
+
+      // Scroll to top on component mount
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
       });
-    }, 0);
+    }
   }, []);
 
   useEffect(() => {
     const phoneTimer = setTimeout(() => {
       setPhoneVisible(true);
-    }, 300); // Delay before the phone and cards start scrolling up
+    }, 300);
 
     const bannerTimer = setTimeout(() => {
       setShowBanner(true);
-    }, 1500); // Delay to show the banner after the phone and cards scroll up
+    }, 1500);
 
     return () => {
       clearTimeout(phoneTimer);
@@ -59,32 +337,17 @@ const HtmlAnimation = () => {
             return prevIndex;
           }
         });
-      }, 2000); // Time between each card scroll
+      }, 2000);
       return () => clearInterval(scrollTimer);
     }
   }, [showBanner, imageList.length]);
 
   const leftContainer = [
-    {
-      name: 'U.S.',
-      image: US
-    },
-    {
-      name: 'INDIA',
-      image: India
-    },
-    {
-      name: 'BELGIUM',
-      image: Belgium
-    },
-    {
-      name: 'UAE',
-      image: Dubai
-    },
-    {
-      name: 'HONG KONG',
-      image: Hongkong
-    }
+    { name: 'U.S.', image: US },
+    { name: 'INDIA', image: India },
+    { name: 'BELGIUM', image: Belgium },
+    { name: 'UAE', image: Dubai },
+    { name: 'HONG KONG', image: Hongkong }
   ];
 
   return (
@@ -92,18 +355,14 @@ const HtmlAnimation = () => {
       <div className="flex justify-between relative xl:px-[112px] lg:px-[32px]">
         {/* Left div */}
         <div
-          className={`flex flex-col gap-4  w-[250px] transition-opacity duration-700 ease-in-out transform ${
+          className={`flex flex-col gap-4 w-[250px] transition-opacity duration-700 ease-in-out transform ${
             showBanner
               ? 'opacity-100 scale-100 translate-x-0'
               : 'opacity-0 scale-90 translate-x-1/2'
           }`}
           style={{
             marginBottom:
-              window && window?.innerWidth >= 1600
-                ? '45%'
-                : window?.innerWidth >= 1280
-                ? '65%'
-                : '75%',
+              windowWidth >= 1600 ? '45%' : windowWidth >= 1280 ? '65%' : '75%',
             marginTop: 'auto'
           }}
         >
@@ -124,45 +383,28 @@ const HtmlAnimation = () => {
             ))}
           </div>
         </div>
-        {/* // phoneVisible
-            //   ? 'translate-y-[-210px] 2xl:translate-y-[-210px] lg:translate-y-[-290px] 1600px:translate-y-[-180px] 1920px:translate-y-[-180px]'
-            //   : // ? ' lg:translate-y-[-262px]  xl:translate-y-[-200px] 2xl:translate-y-[-200px] 3xl:translate-y-[-60px]'
-                // 'translate-y-[calc(1200px - 100vh)]'
-          // } */}
+
         {/* Phone Skeleton */}
         <div
-          className={`relative w-[460px] h-[1200px] bg-no-repeat bg-contain transition-transform duration-700 ease-in-out z-10  flex justify-center`}
+          className={`relative w-[460px] h-[1200px] bg-no-repeat bg-contain transition-transform duration-700 ease-in-out z-10 flex justify-center`}
           style={{
             backgroundImage: `url('/v3/home/phone-skeleton.png')`,
-            zIndex: '10 !important', // Ensure phone skeleton is on top
-            // transform: `${
-            //   phoneVisible
-            //     ? 'translateY(calc(100vh - 1000px))'
-            //     : 'translateY(100vh)'
-            // }`
-            transform:
-              phoneVisible && window
-                ? window?.innerWidth >= 1280 // xl breakpoint (1280px and above)
-                  ? 'translateY(calc(100vh - 950px))'
-                  : window?.innerWidth >= 1024 // lg breakpoint (1024px and above)
-                  ? 'translateY(calc(100vh - 895px))'
-                  : 'translateY(calc(100vh - 800px))' // Default for smaller screens
-                : 'translateY(100vh)'
+            zIndex: '10 !important',
+            transform: phoneVisible
+              ? windowWidth >= 1280
+                ? 'translateY(calc(100vh - 950px))'
+                : windowWidth >= 1024
+                ? 'translateY(calc(100vh - 895px))'
+                : 'translateY(calc(100vh - 800px))'
+              : 'translateY(100vh)'
           }}
         >
           <div
-            className={`absolute  transform -translate-x-1/2 w-[370px] h-[700px] overflow-hidden transition-transform duration-700 ease-in-out z-0 flex flex-col items-center`}
+            className={`absolute transform -translate-x-1/2 w-[370px] h-[700px] overflow-hidden transition-transform duration-700 ease-in-out z-0 flex flex-col items-center`}
             style={{
-              zIndex: -1, // Ensure cards are beneath the phone skeleton
-
+              zIndex: -1,
               transform: phoneVisible
-                ? //   ? window.innerWidth >= 1280 // xl breakpoint (1280px and above)
-                  //     ? 'translateY(calc(100vh - 350px))'
-                  //     : window.innerWidth >= 1024 // lg breakpoint (1024px and above)
-                  //     ? 'translateY(calc(100vh - 250px))'
-                  //     : 'translateY(calc(100vh - 410px))' // Default for smaller screens
-                  //   :
-                  'translateY(385px)'
+                ? 'translateY(385px)'
                 : 'translateY(100vh)'
             }}
           >
@@ -171,7 +413,7 @@ const HtmlAnimation = () => {
               style={{
                 transform: `translateY(-${
                   scrollIndex * (100 / imageList.length)
-                }%)` // Adjust scroll position
+                }%)`
               }}
             >
               {imageList.map((src, index) => (
@@ -189,12 +431,6 @@ const HtmlAnimation = () => {
           </div>
         </div>
 
-        {/* Scrolling Cards */}
-        {/* lg:top-[120px] xl:top-[180px] 2xl:top-[180px] 3xl:top-[330px]  */}
-        {/* ${
-            phoneVisible ? 'translate-y-[120px]' : 'translate-y-full'
-          } */}
-
         {/* Right div */}
         <div
           className={`text-neutral900 w-[250px] transition-opacity duration-700 ease-in-out transform ${
@@ -204,21 +440,11 @@ const HtmlAnimation = () => {
           }`}
           style={{
             marginBottom:
-              window && window?.innerWidth >= 1600
-                ? '45%'
-                : window?.innerWidth >= 1280
-                ? '65%'
-                : '75%',
-
+              windowWidth >= 1600 ? '45%' : windowWidth >= 1280 ? '65%' : '75%',
             marginTop: 'auto'
           }}
         >
-          <div className=" flex flex-col  text-neutral900">
-            {/* <span className="text-[48px] font-semiBold">2.0</span> */}
-            {/* <span className="text-[20px]">
-              Points
-              <br /> below
-            </span>{' '} */}
+          <div className="flex flex-col text-neutral900">
             <span
               className="text-[48px] font-semiBold w-fit"
               style={{
@@ -243,7 +469,6 @@ const HtmlAnimation = () => {
           showBanner ? 'translate-y-0' : 'translate-y-full'
         }`}
         style={{
-          // height: '280px',
           zIndex: 10,
           boxShadow: '0 4px 20px rgba(44, 110, 110, 0.38)'
         }}
@@ -252,7 +477,7 @@ const HtmlAnimation = () => {
           <h1 className="xl:text-[52px] lg:text-[42px] text-neutral900 font-black leading-[48px]">
             Access KGK’s Global Diamond Inventory
           </h1>
-          <p className="xl:text-[36px] lg:text-[28px] text-neutral900  mb-[20px] font-semiBold">
+          <p className="xl:text-[36px] lg:text-[28px] text-neutral900 mb-[20px] font-semiBold">
             Create a Guest Account
           </p>
           <Image
@@ -261,12 +486,6 @@ const HtmlAnimation = () => {
             onClick={() => router.push('/v2/register')}
             className="cursor-pointer lg:w-[224px]"
           />
-          {/* <ShimmerButton
-            className="!rounded-[8px] w-[200px] h-[44px]"
-            onClick={() => router.push('/v2/register')}
-          >
-            Explore Now!
-          </ShimmerButton> */}
         </div>
       </div>
     </div>
