094d8cf3a603a7847562a02acd60796896fcc6d8
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-06 15:04:16 +0530

    logger setup

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,10 @@
+
 'use client'; // Error components must be Client Components
+// import { LogLevel } from "@/next-axiom/logger";
+import { useLogger } from "next-axiom";
+import { usePathname } from "next/navigation";
 
 import CustomErrorComponent from '@/components/v2/common/error';
-import { useEffect } from 'react';
 
 export default function Error({
   error,
@@ -10,10 +13,30 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+  console.log("---------------------------->>>>>>>>>",error)
+
+//   const pathname = usePathname()
+//   const log = useLogger({ source: "error.tsx" });
+//   let status =  error.message == 'Invalid URL' ? 404 : 500;
+//   log.logHttpRequest(
+//     3, //indicating error as per logHttpRequest
+//     error.message,
+//     {
+//       host: window.location.href,
+//       path: pathname,
+//       statusCode: status,
+//     },
+//     {
+//       error: error.name,
+//       cause: error.cause,
+//       stack: error.stack,
+//       digest: error.digest,
+//     },
+//   );
+  // useEffect(() => {
+  //   // Log the error to an error reporting service
+  //   console.error(error);
+  // }, [error]);
 
   return <CustomErrorComponent reset={reset} />;
 }
