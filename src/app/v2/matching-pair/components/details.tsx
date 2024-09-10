118b72dce5f1242ef996ea9d83b739dee8ab158d
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-10 22:38:37 +0530

    console removed

--- src/app/v2/matching-pair/components/details.tsx
+++ src/app/v2/matching-pair/components/details.tsx
@@ -141,22 +141,6 @@ export function MatchPairDetails({
     });
   };
 
-  // useEffect(() => {
-  //   if ('serviceWorker' in navigator) {
-  //     navigator.serviceWorker.register('/service-worker.js').then(
-  //       registration => {
-  //         console.log(
-  //           'Service Worker registered with scope:',
-  //           registration.scope
-  //         );
-  //       },
-  //       error => {
-  //         console.error('Service Worker registration failed:', error);
-  //       }
-  //     );
-  //   }
-  // }, []);
-
   useEffect(() => {
     if (originalData.length >= 2) {
       triggerSimilarMatchingPairApi({
