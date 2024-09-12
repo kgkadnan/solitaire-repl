fedd2d8df4bb76eef4681d4e2677c5aa056b42dc
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-12 18:27:32 +0530

    roundeed radius attempt

--- src/components/v3/post-header.tsx
+++ src/components/v3/post-header.tsx
@@ -1,7 +1,7 @@
 import Date from './date';
 import CoverImage from './cover-image';
 
-export default function PostHeader({ title, coverImage, date }: any) {
+export default function PostHeader({ title, date }: any) {
   return (
     <div className="flex flex-col w-full justify-center">
       <div className="max-w-2xl mx-auto flex w-full justify-center">
@@ -12,9 +12,9 @@ export default function PostHeader({ title, coverImage, date }: any) {
       <div className="flex flex-col w-full justify-around text-center text-headingXL">
         {title}
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
+      {/* <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} coverImage={coverImage} />
-      </div>
+      </div> */}
     </div>
   );
 }
