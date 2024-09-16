f0f533d87cdc0e7f8b1c3f350812a39a759323b9
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-16 13:07:00 +0530

    publish metadata position moved below heading

--- src/components/v3/post-header.tsx
+++ src/components/v3/post-header.tsx
@@ -4,13 +4,13 @@ import Date from './date';
 export default function PostHeader({ title, date }: any) {
   return (
     <div className="flex flex-col max-w-[42rem] mx-auto justify-center">
-      <div className="max-w-2xl mx-auto flex w-full justify-center">
-        <div className="mb-6 text-lg mt-[100px]">
+      <div className="flex flex-col w-full justify-around text-[30px] mt-[100px]">
+        {title}
+      </div>
+      <div className="max-w-2xl mx-auto flex w-full">
+        <div className="mb-6 text-lg ">
           Published <Date dateString={date} />
         </div>
-      </div>
-      <div className="flex flex-col w-full justify-around text-[30px]">
-        {title}
       </div>
       {/* <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} coverImage={coverImage} />
