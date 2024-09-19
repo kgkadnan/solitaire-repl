7c5eeec1f97e42376a5442d00d6d4cbb4fd6d28d
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-19 15:35:13 +0530

    user info tracking added

--- logging/log-util.ts
+++ logging/log-util.ts
@@ -1,3 +1,74 @@
+// // src/logging/log-util.ts
+
+// import * as Sentry from '@sentry/nextjs';
+
+// type LogLevel = 'error' | 'warn' | 'info' | 'debug';
+
+// const levels: Record<LogLevel, number> = {
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   debug: 3
+// };
+
+// let currentLevel: number = levels.info;
+
+// export function setLogLevel(level: LogLevel): void {
+//   if (levels[level] !== undefined) {
+//     currentLevel = levels[level];
+//   }
+// }
+
+// export function log(level: LogLevel, message: any): void {
+//   if (levels[level] <= currentLevel) {
+//     // console[level](message);
+
+//     if (message) {
+//       // Ensure message is not null or undefined
+//       switch (level) {
+//         case 'error':
+//           Sentry.captureException(new Error(message));
+//           break;
+//         case 'warn':
+//           Sentry.captureMessage(message, 'warning');
+//           break;
+//         case 'info':
+//           Sentry.captureMessage(message, 'info');
+//           break;
+//         case 'debug':
+//           Sentry.captureMessage(message, 'debug');
+//           break;
+//       }
+//     } else {
+//       console.error('Log message is null or undefined');
+//     }
+//   }
+// }
+
+// export function error(message: any): void {
+//   log('error', message);
+// }
+
+// export function warn(message: any): void {
+//   log('warn', message);
+// }
+
+// export function info(message: any): void {
+//   log('info', message);
+// }
+
+// export function debug(message: any): void {
+//   log('debug', message);
+// }
+
+// export default {
+//   setLogLevel,
+//   error,
+//   warn,
+//   info,
+//   debug
+// };
+
 // src/logging/log-util.ts
 
 import * as Sentry from '@sentry/nextjs';
@@ -19,12 +90,35 @@ export function setLogLevel(level: LogLevel): void {
   }
 }
 
+function getUserFromLocalStorage() {
+  try {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  } catch (error) {
+    console.error('Error parsing user data from localStorage:', error);
+    return null;
+  }
+}
+
 export function log(level: LogLevel, message: any): void {
   if (levels[level] <= currentLevel) {
+    // Optionally log to console
     // console[level](message);
 
     if (message) {
-      // Ensure message is not null or undefined
+      const user = getUserFromLocalStorage();
+
+      // Set user information in Sentry
+      if (user) {
+        Sentry.setUser({
+          id: user.customer.id,
+          username: user.customer.first_name,
+          email: user.customer.email
+        });
+      } else {
+        Sentry.setUser(null); // Clear user data if not available
+      }
+
       switch (level) {
         case 'error':
           Sentry.captureException(new Error(message));
