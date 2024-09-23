06f936109e4df3274b198af1033a2fe3209d7dbc
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-23 18:28:49 +0530

    traceability changes

--- src/app/v3/traceability/page.tsx
+++ src/app/v3/traceability/page.tsx
@@ -1,6 +1,11 @@
 'use client';
+import {
+  leadingPrograms,
+  traceabilityMatters
+} from '@/constants/v3/traceability';
 import React from 'react';
-
+import Image from 'next/image';
+import TraceabilityHtml from '@/components/v3/traceability-html';
 export default function Traceability() {
   return (
     <div>
@@ -24,6 +29,66 @@ export default function Traceability() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center xl:px-[112px] lg:px-[32px] pb-[40px]">
+        <div className="xl:w-[1100px] lg:w-[950px]  ">
+          <div className="text-neutral900 text-[52px] font-bold text-center leading-[110px] custom-fadeIn">
+            Why Diamond Traceability Matters?
+          </div>
+          <div className="flex flex-wrap gap-[60px] items-center justify-center">
+            {traceabilityMatters.map(matter => (
+              <div className="flex flex-col items-center w-[400px] gap-4">
+                <Image src={matter.icon} alt={matter.heading} />
+                <p className='text-neutral900 text-[20px]'>{matter.heading}</p>
+                <p className="text-justify text-[16px] text-neutral800 ">{matter.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="xl:px-[112px] lg:px-[32px]  pb-[40px] flex flex-col bg-animated-gradient bg-[length:200%_200%] bg-no-repeat animate-gradient items-center blur-bottom  justify-center">
+        <div className="flex flex-col gap-2 text-center w-[840px] py-[40px] justify-center">
+          <div className="flex flex-col gap-2">
+            <p className="xl:text-[52px] lg:text-[42px] text-neutral900 font-black">
+              How Traceability Works in KGK{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center w-full h-fit">
+        <TraceabilityHtml />
+      </div>
+
+      <div className="flex flex-col items-center xl:px-[112px] lg:px-[32px] pb-[40px]">
+        <div className="w-[1200px]">
+          <div className="text-neutral900 text-[52px] font-bold text-center leading-[110px] custom-fadeIn">
+            Leading Traceability Programs We use
+          </div>
+          <div className="flex flex-wrap gap-[60px] items-center justify-center">
+            {leadingPrograms.map(program => (
+              <div className="flex flex-col w-[560px] items-center gap-4">
+                <div
+                  className="w-[560px] h-[240px] rounded-[12px] bg-white flex items-center justify-center"
+                  style={{ boxShadow: 'var(--popups-shadow' }}
+                >
+                  {' '}
+                  <Image
+                    src={program.icon}
+                    alt={program.heading}
+                    className="rounded-[12px]"
+                  />
+                </div>
+                <div
+                  className="w-[528px] rounded-[8px] bg-white p-2 mt-[-120px] h-[220px] gap-2 flex flex-col"
+                  style={{ boxShadow: 'var(--popups-shadow' }}
+                >
+                  <p className='text-neutral900 text-[28px]'>{program.heading}</p>
+                  <p className='text-neutral800 text-[16px]'>{program.description}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/constants/v3/traceability.ts
@@ -0,0 +1,58 @@
+import Trace1 from '@public/v3/traceability/ethical.svg';
+import Trace2 from '@public/v3/traceability/transparency.svg';
+import Trace3 from '@public/v3/traceability/quality.svg';
+import Trace4 from '@public/v3/traceability/environment.svg';
+import Program1 from '@public/v3/traceability/program1.png';
+
+export const traceabilityMatters = [
+  {
+    icon: Trace1,
+    heading: 'Ethical Sourcing',
+    description:
+      'Traceability confirms that diamonds are sourced from conflict-free regions, adhering to ethical labor practices. This helps in preventing the circulation of "blood diamonds" which are often associated with human rights abuses and conflict financing.'
+  },
+  {
+    icon: Trace2,
+    heading: 'Transparency',
+    description: `With traceability, every step of a diamond's journey is documented, providing transparency in the supply chain. This includes details on mining, cutting, polishing, and distribution, ensuring that consumers know exactly where their diamonds come from.`
+  },
+  {
+    icon: Trace3,
+    heading: 'Quality Assurance',
+    description:
+      'Traceability systems often include rigorous quality checks at each stage. This ensures that the diamonds meet high standards for quality and authenticity, giving businesses and consumers confidence in the product they are buying.'
+  },
+  {
+    icon: Trace4,
+    heading: 'Environmental Responsibility',
+    description:
+      'Companies committed to traceability are also often committed to sustainable practices. This includes minimizing environmental impact and supporting initiatives that benefit local communities in mining areas.'
+  }
+];
+
+export const leadingPrograms = [
+  {
+    icon: Program1,
+    heading: 'GIA Source Verification Service (SVS)',
+    description:
+      "The GIA SVS provides an additional layer of assurance by verifying the country of origin and the entire journey of a diamond. This service uses a combination of advanced scientific techniques and a rigorous chain of custody protocols to ensure the integrity and transparency of the diamond's origin and history. Discover GIA SVS."
+  },
+  {
+    icon: Program1,
+    heading: 'DB Origin & Origin Story by De Beers',
+    description:
+      'The Origin suite of services from De Beers includes Origin and Grading Reports and a digital look-up to connect clients with the origin and impact of their diamond. The Origin Story digital experience supports retailers with immersive storytelling, celebrating the uniqueness and personal journey of each diamond.'
+  },
+  {
+    icon: Program1,
+    heading: 'Tracr by De Beers',
+    description:
+      'A blockchain-based platform that records the provenance and journey of diamonds, ensuring secure and transparent traceability. Read more on Tracr'
+  },
+  {
+    icon: Program1,
+    heading: 'Sarine Diamond Journey',
+    description:
+      'Uses advanced scanning and mapping technologies to create a unique digital fingerprint for each diamond, ensuring complete traceability from rough to polished stages. Explore Sarine Diamond Journey'
+  }
+];
