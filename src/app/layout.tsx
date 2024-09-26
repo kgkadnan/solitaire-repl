4f43a6dbfa0e8e89ba76b412329e82b420702b3d
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-09-26 15:42:03 +0530

    rerouting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,32 @@ export default function RootLayout({ children }: { children?: ReactNode }) {
   }, []);
 
   useEffect(() => {
+    // Define the URLs
+    const appStoreURL =
+      'https://apps.apple.com/in/app/kgk-diamond/id6479595403';
+    const playStoreURL =
+      'https://play.google.com/store/apps/details?id=com.kgk.diamonds&hl=en_IN';
+    const desktopURL = 'https://kgkdiamonds.com/';
+
+    // Check if the device is iOS
+    const isIOS = () => /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    // Check if the device is Android
+    const isAndroid = () => /Android/i.test(navigator.userAgent);
+
+    // Perform the redirect based on the platform
+    if (path.includes('/v3') || path.includes('/v2') || path === '/') {
+      if (isIOS()) {
+        window.location.href = appStoreURL;
+      } else if (isAndroid()) {
+        window.location.href = playStoreURL;
+      } else {
+        window.location.href = desktopURL;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     const handleBeforeUnload = () => {
       // Remove the specific localStorage item
       localStorage.removeItem('entryPoint');
