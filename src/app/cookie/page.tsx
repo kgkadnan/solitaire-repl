54600d0c1f2d2d07e6578271187fcefc0f776d5a
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-10-25 17:30:51 +0530

    cookie declaration page

---
+++ src/app/cookie/page.tsx
@@ -0,0 +1,15 @@
+// pages/cookie-declaration.js
+import Head from 'next/head';
+
+export default function CookieDeclaration() {
+  return (
+    <div>
+      <script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/86ce1cb4-4338-418c-acca-d54a1b81cccc/cd.js"
+        type="text/javascript"
+        async
+      ></script>
+    </div>
+  );
+}
