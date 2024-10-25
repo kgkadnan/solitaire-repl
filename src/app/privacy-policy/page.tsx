031a3e028b20f2fe151b2a94f9781df807b905e9
jyotitiwarikgk <jyoti.tiwari@kgkmail.com>
2024-10-25 16:51:48 +0530

    declaration update

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -99,6 +99,12 @@ const PrivacyPolicy = () => {
               />
             </div>
           </div>
+          <script
+            id="CookieDeclaration"
+            src="https://consent.cookiebot.com/86ce1cb4-4338-418c-acca-d54a1b81cccc/cd.js"
+            type="text/javascript"
+            async
+          ></script>
         </>
       ) : (
         <CustomKGKLoader />
