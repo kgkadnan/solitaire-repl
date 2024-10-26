e67ef7e85186e972cfea8201bdd855930f497f4b
kgkadnan <adnan.badshah@kgkmail.com>
2024-10-26 18:51:54 +0530

    fixes

--- src/app/v2/matching-pair/page.tsx
+++ src/app/v2/matching-pair/page.tsx
@@ -10,7 +10,8 @@ import ActionButton from '@/components/v2/common/action-button';
 import { ManageLocales } from '@/utils/v2/translate';
 import { useModalStateManagement } from '@/hooks/v2/modal-state.management';
 import { DialogComponent } from '@/components/v2/common/dialog';
-
+import Cross from '@public/v2/assets/icons/cross.svg?url';
+import Drag from '@public/v2/assets/icons/drag.svg';
 import { useAddSavedSearchMutation } from '@/features/api/saved-searches';
 import { InputDialogComponent } from '@/components/v2/common/input-dialog';
 import { InputField } from '@/components/v2/common/input-field';
@@ -25,11 +26,101 @@ import useValidationStateManagement from '../search/hooks/validation-state-manag
 import SavedSearch from '../search/saved-search/saved-search';
 import MatchingPairResult from './result';
 import {
+  useApplyMatchingPairSettingMutation,
   useGetMatchingPairCountQuery,
-  useLazyGetMatchingPairSettingQuery
+  useLazyGetMatchingPairSettingQuery,
+  useLazyGetResetMatchingPairSettingQuery
 } from '@/features/api/match-pair';
 import CustomKGKLoader from '@/components/v2/common/custom-kgk-loader';
 import { MPSDialogComponent } from './components/mps';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import CommonPoppup from '../login/component/common-poppup';
+import CheckboxComponent from '@/components/v2/common/checkbox';
+import { IndividualActionButton } from '@/components/v2/common/action-button/individual-button';
+const defaultMatchPairSetting = [
+  {
+    up: 0,
+    key: 'color',
+    down: 0,
+    display: 'Color',
+    is_equal: true,
+    priority: 1,
+    start: 0,
+    end: 10
+  },
+  {
+    up: 1,
+    key: 'clarity',
+    down: 1,
+    display: 'Clarity',
+    is_equal: false,
+    priority: 2,
+    start: 0,
+    end: 10
+  },
+  {
+    up: 0.05,
+    key: 'length',
+    down: 0.05,
+    display: 'Length',
+    is_equal: false,
+    priority: 3,
+    start: 0,
+    end: 100
+  },
+  {
+    up: 0.05,
+    key: 'width',
+    down: 0.05,
+    display: 'Width',
+    is_equal: false,
+    priority: 4,
+    start: 0,
+    end: 100
+  },
+  {
+    up: 2,
+    key: 'table_percentage',
+    down: 2,
+    display: 'Table%',
+    is_equal: false,
+    priority: 5,
+    start: 0,
+    end: 100
+  },
+  {
+    up: 1.7,
+    key: 'depth_percentage',
+    down: 1.7,
+    display: 'Depth%',
+    is_equal: false,
+    priority: 6,
+    start: 0,
+    end: 100
+  },
+  {
+    up: 0,
+    key: 'fluorescence',
+    down: 0,
+    display: 'Fluorescence',
+    is_equal: true,
+    priority: 7,
+    start: 0,
+    end: 10
+  },
+  {
+    up: 0.05,
+    key: 'carats',
+    down: 0.05,
+    display: 'Carats',
+    is_equal: false,
+    priority: 8,
+    range_to: 50,
+    range_from: 5,
+    start: 0,
+    end: 100
+  }
+];
 
 const MatchingPair = () => {
   const subRoute = useSearchParams().get('active-tab');
@@ -40,7 +131,8 @@ const MatchingPair = () => {
   const [searchParameters, setSearchParameters] = useState<ISavedSearch[] | []>(
     []
   );
-
+  const [resetMPS] = useLazyGetResetMatchingPairSettingQuery({});
+  const [applyMPS] = useApplyMatchingPairSettingMutation({});
   const { setSearchUrl, searchUrl, addSearches, setAddSearches } =
     useValidationStateManagement();
   const { modalState, modalSetState } = useModalStateManagement();
@@ -74,22 +166,11 @@ const MatchingPair = () => {
   const [mps, setMps] = useState([
     {
       up: 0,
-      key: 'shape',
-      down: 0,
-      display: 'Shape',
-      is_equal: true,
-      priority: 1,
-      start: 0,
-      end: 10,
-      placeHolder: '0'
-    },
-    {
-      up: 0,
       key: 'color',
       down: 0,
       display: 'Color',
       is_equal: true,
-      priority: 2,
+      priority: 1,
       start: 0,
       end: 10,
       placeHolder: '0'
@@ -100,7 +181,7 @@ const MatchingPair = () => {
       down: 0,
       display: 'Clarity',
       is_equal: false,
-      priority: 3,
+      priority: 2,
       start: 0,
       end: 10,
       placeHolder: '0'
@@ -111,7 +192,7 @@ const MatchingPair = () => {
       down: 0.0,
       display: 'Length',
       is_equal: false,
-      priority: 4,
+      priority: 3,
       start: 0,
       end: 100,
       placeHolder: '0.00'
@@ -122,7 +203,7 @@ const MatchingPair = () => {
       down: 0.0,
       display: 'Width',
       is_equal: false,
-      priority: 5,
+      priority: 4,
       start: 0,
       end: 100,
       placeHolder: '0.00'
@@ -133,7 +214,7 @@ const MatchingPair = () => {
       down: 0.0,
       display: 'Table %',
       is_equal: false,
-      priority: 6,
+      priority: 5,
       start: 0,
       end: 100,
       placeHolder: '0.00'
@@ -144,7 +225,7 @@ const MatchingPair = () => {
       down: 0.0,
       display: 'Depth %',
       is_equal: false,
-      priority: 7,
+      priority: 6,
       start: 0,
       end: 100,
       placeHolder: '0.00'
@@ -155,7 +236,7 @@ const MatchingPair = () => {
       down: 0,
       display: 'Fluorescence',
       is_equal: true,
-      priority: 8,
+      priority: 7,
       start: 0,
       end: 10,
       placeHolder: '0'
@@ -167,7 +248,7 @@ const MatchingPair = () => {
       down: 0.0,
       display: 'Carat',
       is_equal: false,
-      priority: 9,
+      priority: 8,
       range_to: 50,
       range_from: 5,
       start: 0,
@@ -175,6 +256,11 @@ const MatchingPair = () => {
       placeHolder: '0.00'
     }
   ]);
+  const [isMPSOpen, setIsMPSOpen] = useState(false);
+  const [initialMps, setInitialMps] = useState(mps); // Store the initial MPS state
+  const [isModified, setIsModified] = useState(false); // Track whether there are any changes
+  const [initialMpsState, setInitialMpsState] = useState(mps); // Store the initial MPS state
+  const [settingApplied, setSettingApplied] = useState(false);
 
   useEffect(() => {
     let selection = JSON.parse(localStorage.getItem('MatchingPair')!) || [];
@@ -323,6 +409,314 @@ const MatchingPair = () => {
       setInputError('Input cannot exceed 20 characters');
     }
   };
+
+  const handleResetMPS = () => {
+    resetMPS({})
+      .unwrap()
+      .then(res => {
+        const resMap = new Map(res.map((itemB: any) => [itemB.key, itemB]));
+
+        const updatedMps = mps.map((itemA: any) => {
+          const itemB: any = resMap.get(itemA.key);
+
+          if (itemB) {
+            const updatedItem = { ...itemA };
+            Object.keys(itemB).forEach(key => {
+              if (itemA[key] !== itemB[key]) {
+                updatedItem[key] = itemB[key];
+              }
+            });
+            return updatedItem;
+          }
+
+          return itemA;
+        });
+
+        updatedMps.sort((a: any, b: any) => a.priority - b.priority);
+
+        console.log('updatedMps', updatedMps);
+
+        // Update state with the new array and reset isModified
+        setMps(updatedMps);
+        setInitialMps(updatedMps); // Update the initial state to the new reset state
+        setIsModified(false); // Disable the buttons
+        console.log(res, 'Reset data');
+        setSettingApplied(!settingApplied);
+      });
+  };
+
+  // Function to compare the current MPS state with the initial state
+  const checkForChanges = (currentMps: any[]) => {
+    console.log('currentMps', currentMps);
+    return JSON.stringify(currentMps) !== JSON.stringify(initialMps);
+  };
+  // Function to compare the current MPS state with the initial state
+  const isDefaultSetting = () => {
+    return JSON.stringify(mps) !== JSON.stringify(defaultMatchPairSetting);
+  };
+
+  const handleApplyMPS = () => {
+    applyMPS({ setting: mps }).unwrap();
+    setIsMPSOpen(false);
+    setInitialMps(mps); // Set the current MPS as the new initial state after applying changes
+    setIsModified(false); // Disable the buttons
+    setSettingApplied(!settingApplied);
+  };
+
+  const handleMPSInputChange = (
+    index: number,
+    newValue: string,
+    field: string
+  ) => {
+    // Allow input to be freely typed, even with leading zeros
+    const updatedMps = [...mps];
+    updatedMps[index] = { ...updatedMps[index], [field]: newValue }; // Keep as string while typing
+    setMps(updatedMps);
+    setIsModified(checkForChanges(updatedMps));
+  };
+
+  const handleOnDragEnd = (result: any) => {
+    if (!result.destination) return;
+
+    const newItems = Array.from(mps);
+    const [reorderedItem] = newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index + 1, 0, reorderedItem);
+
+    // Update priority based on new position
+    const updatedItems = newItems.map((item: any, index) => ({
+      ...item,
+      priority: index + 1 // Priority is set to index+1 to reflect the new order
+    }));
+
+    setMps(updatedItems);
+    setIsModified(true);
+  };
+
+  const handleInputBlur = (index: number, field: string) => {
+    const endValue = mps[index].end;
+    // @ts-ignore
+    let value = parseFloat(mps[index][field]) || 0; // Convert to number on blur
+
+    // Validate the value
+    if (value < 0) value = 0;
+    if (value > endValue) value = endValue;
+
+    // If endValue is 10, ensure the input is an integer
+    if (endValue === 10) {
+      value = Math.round(value); // Round to nearest integer
+    } else {
+      // If endValue is not 10, allow up to 2 decimal places
+      value = parseFloat(value.toFixed(2)); // Limit to 2 decimal places
+    }
+
+    const updatedMps = [...mps];
+    updatedMps[index] = { ...updatedMps[index], [field]: value.toString() }; // Set final validated value as string
+    setMps(updatedMps);
+  };
+  const handleIsEqualChange = (index: number) => {
+    const updatedMps = [...mps];
+
+    updatedMps[index] = {
+      ...updatedMps[index],
+      is_equal: !updatedMps[index].is_equal,
+      // @ts-ignore
+      up: updatedMps[index].end === 100 ? '0.00' : '0',
+      // @ts-ignore
+      down: updatedMps[index].end === 100 ? '0.00' : '0'
+    };
+
+    setMps(updatedMps);
+    setIsModified(checkForChanges(updatedMps)); // Check if the state has been modified
+  };
+
+  const renderContentMPS = () => {
+    return (
+      <div>
+        <div className="flex justify-between w-full p-4 items-center">
+          <p className="text-headingS font-medium text-neutral900">
+            Match Pair Settings
+          </p>
+          <div
+            className="cursor-pointer"
+            onClick={() => {
+              if (isModified) {
+                modalSetState.setIsDialogOpen(true);
+                modalSetState.setDialogContent(
+                  <CommonPoppup
+                    content={
+                      'You have unsaved changes. Are you sure you want to exit?'
+                    }
+                    status="warning"
+                    customPoppupBodyStyle="!mt-[70px]"
+                    header="Exit Without Saving?"
+                    actionButtonData={[
+                      {
+                        variant: 'secondary',
+                        label: 'No, Stay',
+                        handler: () => {
+                          modalSetState.setIsDialogOpen(false);
+                        },
+                        customStyle: 'flex-1'
+                      },
+                      {
+                        variant: 'primary',
+                        label: 'Yes, Exit',
+                        handler: () => {
+                          modalSetState.setIsDialogOpen(false);
+                          setIsMPSOpen(false);
+                          setMps(initialMpsState);
+                          setIsModified(false);
+                          setInitialMps(initialMpsState);
+                        },
+                        customStyle: 'flex-1'
+                      }
+                    ]}
+                  />
+                );
+              } else {
+                setIsMPSOpen(false);
+              }
+            }}
+          >
+            <Cross style={{ stroke: 'var(--neutral-900)' }} />
+          </div>
+        </div>
+
+        {/* Header for the table */}
+        <div className="w-full flex justify-between items-center bg-[#F9FAFB] h-[50px] border-t-[1px] border-b-[1px] border-neutral200">
+          <p className="w-[60px] px-2">Priority</p>
+          <p className="w-[150px]">Name</p>
+          <div className="w-[80px] flex justify-center">Equal</div>
+          <p className="w-[67px] flex justify-start">Up</p>
+          <p className="w-[80px] flex justify-start mr-[12px]">Down</p>
+          <div className="w-[87px]">Action</div>
+        </div>
+
+        {/* Main draggable container */}
+        <div className=" relative overflow-auto h-[441px]">
+          {' '}
+          {/* Add scroll and limit height */}
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="droppable">
+              {provided => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {mps.map((item: any, index: number) => (
+                    <div
+                      className={`flex gap-[23px] bg-neutral0 text-[14px] rounded-lg border-b-[1px] `}
+                    >
+                      <p className="w-[60px] flex items-center bg-[#F9FAFB] justify-center">
+                        {item.priority}
+                      </p>
+
+                      <Draggable
+                        key={item.key}
+                        draggableId={item.key.toString()}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            className={`flex gap-[23px]  text-[14px]  ${
+                              snapshot.isDragging ? 'shadow-lg' : ''
+                            }`}
+                          >
+                            <div className="flex gap-[12px] bg-neutral0">
+                              <p className="w-[150px] flex items-center">
+                                {item.display}
+                              </p>
+
+                              <div className="w-[99px] flex items-center justify-center">
+                                <CheckboxComponent
+                                  onClick={() => handleIsEqualChange(index)}
+                                  isChecked={item.is_equal}
+                                />
+                              </div>
+
+                              <div className="w-[80px] py-1">
+                                <InputField
+                                  onChange={e =>
+                                    handleMPSInputChange(
+                                      index,
+                                      e.target.value,
+                                      'up'
+                                    )
+                                  }
+                                  onBlur={() => handleInputBlur(index, 'up')}
+                                  type="number"
+                                  value={item.up}
+                                  placeholder={item.placeHolder}
+                                  styles={{ inputMain: 'h-[40px]' }}
+                                  disabled={item.is_equal}
+                                />
+                              </div>
+
+                              <div className="w-[80px] py-1">
+                                <InputField
+                                  onChange={e =>
+                                    handleMPSInputChange(
+                                      index,
+                                      e.target.value,
+                                      'down'
+                                    )
+                                  }
+                                  onBlur={() => handleInputBlur(index, 'down')}
+                                  type="number"
+                                  value={item.down}
+                                  placeholder={item.placeHolder}
+                                  styles={{ inputMain: 'h-[40px]' }}
+                                  disabled={item.is_equal}
+                                />
+                              </div>
+
+                              <div
+                                className="w-[80px] flex justify-center"
+                                {...provided.dragHandleProps}
+                              >
+                                <Image src={Drag} alt="MPS drag" />
+                              </div>
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    </div>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+
+        {/* Buttons at the bottom */}
+        <div className="flex p-4 h-[56px] items-center gap-4">
+          <IndividualActionButton
+            onClick={handleResetMPS}
+            variant="secondary"
+            size="custom"
+            className="rounded-[4px] w-[100%] h-[40px]"
+            disabled={!isDefaultSetting()}
+          >
+            Reset
+          </IndividualActionButton>
+          <IndividualActionButton
+            onClick={handleApplyMPS}
+            variant="primary"
+            size="custom"
+            className="rounded-[4px] w-[100%] h-[40px]"
+            disabled={!isModified}
+          >
+            Apply
+          </IndividualActionButton>
+        </div>
+      </div>
+    );
+  };
+  // This useEffect sets the initialMps state once MPS data is loaded
+  useEffect(() => {
+    setInitialMps(mps);
+  }, [mps]);
+
   const renderContentWithInput = () => {
     return (
       <>
@@ -405,6 +799,11 @@ const MatchingPair = () => {
 
   return (
     <div>
+      <MPSDialogComponent
+        isOpen={isMPSOpen}
+        onClose={() => setIsMPSOpen(false)}
+        renderContent={renderContentMPS}
+      />
       {isLoading && <CustomKGKLoader />}
       <DialogComponent
         dialogContent={dialogContent}
@@ -443,6 +842,7 @@ const MatchingPair = () => {
           isMatchingPair={true}
           setIsCommonLoading={setIsLoading}
           isLoading={searchLoading}
+          setIsMPSOpen={setIsMPSOpen}
         />
       ) : subRoute === MatchSubRoutes.SAVED_SEARCH ? (
         <SavedSearch setIsLoading={setIsLoading} />
@@ -473,6 +873,9 @@ const MatchingPair = () => {
           isLoading={isLoading}
           mps={mps}
           setMps={setMps}
+          setSettingApplied={setSettingApplied}
+          settingApplied={settingApplied}
+          setIsMPSOpen={setIsMPSOpen}
         />
       )}
     </div>
