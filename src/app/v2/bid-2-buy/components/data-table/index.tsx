f522bd0d274889b2c976c3939babaf13427ff446
kgkadnan <adnan.badshah@kgkmail.com>
2024-11-06 13:41:20 +0530

    fix

--- src/app/v2/bid-2-buy/components/data-table/index.tsx
+++ src/app/v2/bid-2-buy/components/data-table/index.tsx
@@ -31,7 +31,7 @@ import { formatNumber } from '@/utils/fix-two-digit-number';
 import { handleDecrementDiscount } from '@/utils/v2/handle-decrement-discount';
 import { handleIncrementDiscount } from '@/utils/v2/handle-increment-discount';
 import { RenderBidToBuyLotIdColor } from '@/components/v2/common/data-table/helpers/render-cell';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { SubRoutes } from '@/constants/v2/enums/routes';
 import { ManageLocales } from '@/utils/v2/translate';
 import BiddingSkeleton from '@/components/v2/skeleton/bidding';
@@ -228,8 +228,8 @@ const BidToBuyDataTable = ({
     if (globalFilter !== '') {
       // Remove all whitespace characters from globalFilter
       const trimmedFilter = globalFilter.replace(/\s+/g, '');
-      let data = rows.filter(
-        (data: any) => data?.lot_id?.startsWith(trimmedFilter)
+      let data = rows.filter((data: any) =>
+        data?.lot_id?.startsWith(trimmedFilter)
       );
       const startIndex = pagination.pageIndex * pagination.pageSize;
       const endIndex = startIndex + pagination.pageSize;
@@ -922,35 +922,37 @@ const BidToBuyDataTable = ({
             ? 'calc(100vh - 123px)'
             : 'calc(100vh - 175px)'
           : activeTab === 2
-          ? isNudge &&
-            (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
-              isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
-            ? 'calc(100vh - 254px)'
-            : 'calc(100vh - 260px)'
-          : isNudge &&
-            (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
-              isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
-          ? 'calc(100vh - 254px)'
-          : !rows.length
-          ? 'calc(100vh - 260px)'
-          : !rows.length
-          ? 'calc(100vh - 260px)'
-          : 'calc(100vh - 255px)',
+            ? isNudge &&
+              (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
+                isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
+              ? 'calc(100vh - 254px)'
+              : 'calc(100vh - 260px)'
+            : isNudge &&
+                (isKycVerified?.customer?.kyc?.status ===
+                  kycStatus.INPROGRESS ||
+                  isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
+              ? 'calc(100vh - 254px)'
+              : !rows.length
+                ? 'calc(100vh - 260px)'
+                : !rows.length
+                  ? 'calc(100vh - 260px)'
+                  : 'calc(100vh - 255px)',
         maxHeight: isFullScreen
           ? activeTab === 2
             ? 'calc(100vh - 123px)'
             : 'calc(100vh - 175px)'
           : activeTab === 2
-          ? isNudge &&
-            (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
-              isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
-            ? 'calc(100vh - 362px)'
-            : 'calc(100vh - 260px)'
-          : isNudge &&
-            (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
-              isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
-          ? 'calc(100vh - 362px)'
-          : 'calc(100vh - 260px)'
+            ? isNudge &&
+              (isKycVerified?.customer?.kyc?.status === kycStatus.INPROGRESS ||
+                isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
+              ? 'calc(100vh - 362px)'
+              : 'calc(100vh - 260px)'
+            : isNudge &&
+                (isKycVerified?.customer?.kyc?.status ===
+                  kycStatus.INPROGRESS ||
+                  isKycVerified?.customer?.kyc?.status === kycStatus.REJECTED)
+              ? 'calc(100vh - 362px)'
+              : 'calc(100vh - 260px)'
       }
     },
     muiTableHeadRowProps: {
@@ -1121,8 +1123,8 @@ const BidToBuyDataTable = ({
           bidValues[row.id] !== undefined
             ? bidValues[row.id]
             : activeTab === 1
-            ? parseFloat(row.original.my_current_bid).toFixed(2)
-            : parseFloat(row.original.discount).toFixed(2);
+              ? parseFloat(row.original.my_current_bid).toFixed(2)
+              : parseFloat(row.original.discount).toFixed(2);
 
         // If the row is selected, return the detail panel content
         return (
