719234f9fd574cc4054acc5065e71ce653b8e9af
kgkadnan <adnan.badshah@kgkmail.com>
2024-11-14 11:51:46 +0530

    fix landing page

--- src/components/v3/section-3d/html.tsx
+++ src/components/v3/section-3d/html.tsx
@@ -111,13 +111,17 @@ const HtmlAnimation = () => {
           }`}
           style={{
             marginBottom:
-              windowHeight >= 850
-                ? '50%'
+              windowHeight >= 1080
+                ? '28%'
+                : windowHeight >= 900
+                ? '55%'
+                : windowHeight >= 850
+                ? '52%'
                 : windowHeight >= 700
-                  ? '60%'
-                  : windowHeight >= 600
-                    ? '75%'
-                    : '95%',
+                ? '61%'
+                : windowHeight >= 600
+                ? '68%'
+                : '95%',
             marginTop: 'auto'
           }}
         >
@@ -146,13 +150,17 @@ const HtmlAnimation = () => {
             backgroundImage: `url('/v3/home/phone-skeleton.png')`,
             zIndex: '10 !important',
             transform: phoneVisible
-              ? windowHeight <= 768
+              ? windowWidth >= 1536 && windowHeight <= 750
+                ? 'translateY(calc(100vh - 950px))'
+                : windowHeight <= 800
                 ? 'translateY(calc(100vh - 900px))'
+                : windowHeight <= 900
+                ? 'translateY(calc(100vh - 950px))'
                 : windowWidth >= 1280
                 ? 'translateY(calc(100vh - 950px))'
                 : windowWidth >= 1024
-                  ? 'translateY(calc(100vh - 895px))'
-                  : 'translateY(calc(100vh - 800px))'
+                ? 'translateY(calc(100vh - 895px))'
+                : 'translateY(calc(100vh - 800px))'
               : 'translateY(100vh)'
           }}
         >
@@ -197,13 +205,17 @@ const HtmlAnimation = () => {
           }`}
           style={{
             marginBottom:
-              windowHeight >= 850
-                ? '50%'
+              windowHeight >= 1080
+                ? '28%'
+                : windowHeight >= 900
+                ? '55%'
+                : windowHeight >= 850
+                ? '52%'
                 : windowHeight >= 700
-                  ? '60%'
-                  : windowHeight >= 600
-                    ? '75%'
-                    : '95%',
+                ? '61%'
+                : windowHeight >= 600
+                ? '68%'
+                : '95%',
             marginTop: 'auto'
           }}
         >
