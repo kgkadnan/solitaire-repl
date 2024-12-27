ad66733f56678a9849672d1950aa6a24d56bf666
kgkadnan <adnan.badshah@kgkmail.com>
2024-12-27 13:48:56 +0530

    fix global filter issue while navigating

--- src/components/v2/common/data-table/index.tsx
+++ src/components/v2/common/data-table/index.tsx
@@ -216,7 +216,6 @@ const DataTable = ({
   setCompareStoneData,
   setIsInputDialogOpen,
   isDashboard,
-  setIsDetailPage,
   handleCreateAppointment,
   setIsSkeletonLoading,
   isSkeletonLoading,
@@ -1118,6 +1117,7 @@ const DataTable = ({
                 setActiveTab={setActiveTab}
                 handleCloseSpecificTab={handleCloseSpecificTab}
                 setIsLoading={setIsLoading}
+                setGlobalFilter={setGlobalFilter}
               />
             </div>
             <div className="pr-[2px] flex gap-[12px] w-[500px]  justify-end flex-wrap relative">
