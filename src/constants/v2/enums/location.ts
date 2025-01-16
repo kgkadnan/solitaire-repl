05adc0a71f0307154b6ee7894e02aa7d7b7a5dbb
kgkadnan <adnan.badshah@kgkmail.com>
2025-01-16 09:16:21 +0530

    fix

--- src/constants/v2/enums/location.ts
+++ src/constants/v2/enums/location.ts
@@ -2,12 +2,14 @@ export const STONE_LOCATION = {
   IND: 'India',
   DUB: 'Dubai',
   BEL: 'Belgium',
-  USA: 'USA'
+  USA: 'USA',
+  'USA-BD': 'USA-BD'
 };
 
 export const STONE_LOCATION_SHORT = {
   India: 'IND',
   Dubai: 'DUB',
   Belgium: 'BEL',
-  USA: 'USA'
+  USA: 'USA',
+  'USA-BD': 'USA-BD'
 };
